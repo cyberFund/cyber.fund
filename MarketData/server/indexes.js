49fa463787b2bce6a92d325d0f5548ff5932e12e
Nikita Litvin <deltaidea@derpy.ru>
2015-03-18 19:57:06 +0500

    feat(MarketData): add a `unique` index for (timestamp & system name & data source)
    Add a `unique sparse dropDups` composite index for `timestamp`, `name`, and `source` fields.
    This will prevent the server from fetching and saving the same data after restart.
    
    This closes #25.

--- MarketData/server/indexes.js
+++ MarketData/server/indexes.js
@@ -1,3 +1,13 @@
 MarketData._ensureIndex({ timestamp: 1 });
 MarketData._ensureIndex({ name: 1 });
 MarketData._ensureIndex({ source: 1 });
+
+MarketData._ensureIndex({
+	timestamp: 1,
+	name: 1,
+	source: 1,
+}, {
+	unique: true,
+	sparse: true,
+	dropDups: true,
+});
