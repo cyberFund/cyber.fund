02dde5774fe5282a6d469c028d8429c555b0f935
Nikita Litvin <deltaidea@derpy.ru>
2015-03-18 20:31:07 +0500

    fix(MarketData): use name-symbol pairs to distinguish systems
    Neither name, nor symbol are unique, but their pair is. Let's use it in db indexes.

--- MarketData/server/indexes.js
+++ MarketData/server/indexes.js
@@ -1,10 +1,12 @@
 MarketData._ensureIndex({ timestamp: 1 });
-MarketData._ensureIndex({ name: 1 });
+// Neither name, nor symbol are unique, but their pair is.
+MarketData._ensureIndex({ name: 1, symbol: 1 });
 MarketData._ensureIndex({ source: 1 });
 
 MarketData._ensureIndex({
 	timestamp: 1,
 	name: 1,
+	symbol: 1,
 	source: 1,
 }, {
 	unique: true,
