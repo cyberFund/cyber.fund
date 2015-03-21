b217f769f47fda3303d76aad1e5a2c28ea74b8d4
Nikita Litvin <deltaidea@derpy.ru>
2015-03-21 18:25:06 +0500

    feat(rating-table): allow to set offset and limit for systems, show first 10 by default

--- server/MarketData/publish.js
+++ server/MarketData/publish.js
@@ -1,9 +1,17 @@
-Meteor.publish("current-data", function() {
+Meteor.publish("current-data", function(offset, limit) {
+
+	var latestTimestamp = MarketData.findOne({}, {
+		sort: { timestamp: -1 },
+		fields: { timestamp: 1 },
+	}).timestamp;
+
 	return MarketData.find(
-		{},
+		{ timestamp: latestTimestamp },
 		{
 			sort: { timestamp: -1 },
-			limit: 10,
+			limit: limit ? limit : 10,
+			offset: offset ? offset : 0
 		}
 	);
+
 });
