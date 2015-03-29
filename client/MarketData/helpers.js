76d0c9208080aa3cc83653e505f83e682f90195b
Litvin Nikita <deltaidea@derpy.ru>
2015-03-29 18:55:31 +0500

    feat(rating-table): use `reactive-table` package
    This closes #32 and is related to #36.

--- client/MarketData/helpers.js
+++ client/MarketData/helpers.js
@@ -4,10 +4,94 @@ Meteor.startup(function() {
 		this.subscribe("current-data");
 	});
 
+	var parseNumber = function(stringOrNumber) {
+		var asFloat = parseFloat(stringOrNumber);
+		var asInt = isNaN(asFloat) ? 0 : Math.round(asFloat);
+		return asInt;
+	};
+
+	var formatNumber = function(stringOrNumber) {
+		var formatted = parseNumber(stringOrNumber).toString()
+			.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		return formatted;
+	};
+
 	Template.marketData.helpers({
-		systems: function() {
-			return MarketData.find({});
-		}
+
+		tableSettings: function() {
+			return {
+				collection: MarketData,
+				showFilter: false,
+				rowsPerPage: Number.MAX_VALUE,
+				showNavigation: "never",
+				fields: [
+					{
+						fieldId: "icon",
+						label: "",
+						sortable: false,
+						tmpl: Template.systemIcon,
+					},
+					{
+						fieldId: "name",
+						key: "name",
+						label: "System",
+					},
+					{
+						fieldId: "rating",
+						key: "metrics.rating",
+						label: "Rating",
+						tmpl: Template.systemRating,
+					},
+					{
+						fieldId: "supply",
+						key: "metrics.availableSupplyNumber",
+						label: "Supply",
+						sort: -1,
+						tmpl: Template.systemSupplyChange,
+					},
+					{
+						fieldId: "volume",
+						key: "metrics.volume24.btc",
+						label: "Trade volume",
+						tmpl: Template.systemTradeVolume,
+						fn: parseNumber,
+					},
+					{
+						fieldId: "btc-cap",
+						key: "metrics.marketCap.btc",
+						label: "Bitcoins cap",
+						tmpl: Template.systemMarketCapBtc,
+						fn: parseNumber,
+					},
+					{
+						fieldId: "btc-cap-change",
+						key: "metrics.marketCapChange.7d.btc",
+						label: "Bitcoins cap change",
+						fn: parseNumber,
+					},
+					{
+						fieldId: "usd-cap",
+						key: "metrics.marketCap.usd",
+						label: "USD cap",
+						tmpl: Template.systemMarketCapUsd,
+						fn: parseNumber,
+					},
+					{
+						fieldId: "usp-cap-change",
+						key: "metrics.marketCapChange.7d.usd",
+						label: "USD cap change",
+						fn: parseNumber,
+					},
+					{
+						fieldId: "chart",
+						label: "",
+						sortable: false,
+						tmpl: Template.systemCapChangeChart,
+					},
+				],
+			};
+		},
+
 	});
 
 });
---
+++ client/lib/helpers.js
@@ -0,0 +1,19 @@
+Meteor.startup(function() {
+
+	var parseNumber = function(stringOrNumber) {
+		var asFloat = parseFloat(stringOrNumber);
+		var asInt = isNaN(asFloat) ? 0 : Math.round(asFloat);
+		return asInt;
+	};
+
+	var formatNumber = function(stringOrNumber) {
+		var formatted = parseNumber(stringOrNumber).toString()
+			.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		return formatted;
+	};
+
+	Template.registerHelper("formatNumber", function(stringOrNumber) {
+		return formatNumber(stringOrNumber);
+	});
+
+});
