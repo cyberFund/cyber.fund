f502a7050e8c4cc00da591623cf967ab90719bd1
Litvin Nikita <deltaidea@derpy.ru>
2015-04-05 00:20:43 +0500

    refactor(processing): refactor `getNearestTimestamp` and cover with tests

--- server/MarketData/processing.js
+++ server/MarketData/processing.js
@@ -4,34 +4,42 @@ this.processing = {
 
 		var query, nearestAfter, nearestBefore;
 
-		if (source) {
-			query = { source: source };
+		var getWithQuery = function(query) {
+
+			var result;
+			var options = { fields: { timestamp: 1 } };
+
+			if (source) {
+				query.source = source;
+			}
+
+			if ("timestamp" in query && "$gte" in query.timestamp) {
+				options.sort = { timestamp: 1 };
+			} else {
+				options.sort = { timestamp: -1 };
+			}
+
+			result = MarketData.findOne(query, options);
+			return result ? result.timestamp : null;
+
+		};
+
+		if (typeof nearestTo === "number" && (nearestTo % 1) === 0) {
+
+			nearestBefore = getWithQuery({ timestamp: { $lt: nearestTo } });
+			nearestAfter = getWithQuery({ timestamp: { $gte: nearestTo } });
+
+			if (nearestAfter && nearestBefore) {
+				return Math.max(nearestBefore, nearestAfter);
+			} else if (nearestAfter) {
+				return nearestAfter;
+			} else {
+				return nearestBefore;
+			}
+
 		} else {
-			query = {};
-		}
 
-		if (nearestTo) {
-
-			query.timestamp = { $lt: nearestTo };
-			nearestBefore = MarketData.findOne(query, {
-				sort: { timestamp: -1 },
-				fields: { timestamp: 1 },
-			}).timestamp;
-
-			query.timestamp = { $gte: nearestTo };
-			nearestAfter = MarketData.findOne(query, {
-				sort: { timestamp: 1 },
-				fields: { timestamp: 1 },
-			}).timestamp;
-
-			return Math.max(nearestBefore, nearestAfter);
-
-		} else {
-
-			return MarketData.findOne(query, {
-				sort: { timestamp: -1 },
-				fields: { timestamp: 1 },
-			}).timestamp;
+			return getWithQuery({});
 
 		}
 
--- tests/jasmine/server/integration/processingSpec.js
+++ tests/jasmine/server/integration/processingSpec.js
@@ -14,11 +14,49 @@ Jasmine.onTest(function() {
 
 			});
 
-			it("should get the last timestamp", function() {
-
+			it("should get the latest timestamp by default", function() {
 				result = processing.getNearestTimestamp();
 				expect(result).toBe(500);
+			});
 
+			it("should get the latest timestamp for a given source", function() {
+				result = processing.getNearestTimestamp("foo");
+				expect(result).toBe(400);
+			});
+
+			it("should get the nearest timestamp to a given one", function() {
+				result = processing.getNearestTimestamp(null, 290);
+				expect(result).toBe(300);
+			});
+
+			it("should get the nearest timestamp to a given one for a given source", function() {
+				result = processing.getNearestTimestamp("bar", 420);
+				expect(result).toBe(500);
+			});
+
+			it("should return null when there's no such source", function() {
+				result = processing.getNearestTimestamp("no such source");
+				expect(result).toBeNull();
+			});
+
+			it("should return null when there's no such source when given a timestamp", function() {
+				result = processing.getNearestTimestamp("no such source", 500);
+				expect(result).toBeNull();
+			});
+
+			it("should act correctly when given a timestamp less than all in the db", function() {
+				result = processing.getNearestTimestamp("foo", 50);
+				expect(result).toBe(100);
+			});
+
+			it("should act correctly when given a timestamp more than all in the db", function() {
+				result = processing.getNearestTimestamp("foo", 1000);
+				expect(result).toBe(400);
+			});
+
+			it("should treat 0 as a correct timestamp", function() {
+				result = processing.getNearestTimestamp("foo", 0);
+				expect(result).toBe(100);
 			});
 
 		});
