af59c672b1b15b6ecc5b5cc4f54617eddecc8d55
Nikita Litvin <deltaidea@derpy.ru>
2015-04-19 20:33:58 +0500

    feat(fetching:cyberFund): add `cyberFund` source

---
+++ server/MarketData/fetching/cyberFund/cyberFund.js
@@ -0,0 +1,48 @@
+var sourceUrl = "http://cyber.fund/data.json";
+var fetchInterval = 30 * 60 * 1000;
+
+this.fetching.cyberFund = {};
+
+this.fetching.cyberFund.processData = function(data, callback) {
+	var timestamp = moment().unix();
+
+	var processedData = data.map(function(system) {
+		system.source = "cyberFund";
+		system.timestamp = timestamp;
+		return system;
+	});
+
+	callback(null, processedData);
+};
+
+Meteor.startup(function() {
+	var fetch = function() {
+		console.log("Fetching data from cyberFund...");
+		fetching.get(sourceUrl, { timeout: fetchInterval }, function(error, getResult) {
+			if (error) {
+				console.error("Error while fetching:", error);
+				return;
+			}
+
+			fetching.cyberFund.processData(getResult, function(error, processResult) {
+				if (error) {
+					console.error("Error while processing:", error);
+					return;
+				}
+
+				fetching.saveToDb(processResult, function(error, saveResult) {
+					if (error) {
+						console.error("Error while saving:", error);
+						return;
+					}
+
+					processing.doPostprocessing("cyberFund", processResult[0].timestamp, processResult);
+					console.log("Saved!");
+				});
+			});
+		});
+	};
+
+	Meteor.setInterval(fetch, fetchInterval);
+	fetch();
+});
