8c5be4949fde22b65191b93c49695b30d88a796c
Nikita Litvin <deltaidea@derpy.ru>
2015-04-19 23:18:16 +0500

    feat(processing:CoinMarketCap): add cap history for a month to `CurrentData`

--- server/MarketData/fetching/CoinMarketCap/postprocessor-CurrentData.js
+++ server/MarketData/fetching/CoinMarketCap/postprocessor-CurrentData.js
@@ -5,6 +5,33 @@ Meteor.startup(function() {
 			return;
 		}
 
+		var monthAgoTimestamp = moment.unix(timestamp).subtract(1, "months").unix();
+
+		var capHistory = {};
+		var capHistoryRaw = MarketData.find({
+			timestamp: { $gt: monthAgoTimestamp },
+			source: "CoinMarketCap",
+		}, {
+			sort: { timestamp: 1 },
+			fields: {
+				timestamp: 1,
+				name: 1,
+				symbol: 1,
+				"metrics.marketCap.btc": 1,
+				"metrics.marketCap.usd": 1,
+			}
+		}).fetch();
+		console.log("capHistoryRaw size:", capHistoryRaw.length);
+
+		capHistoryRaw.forEach(function(rawCap) {
+			var id = rawCap.name + "/" + rawCap.symbol;
+			capHistory[id] = capHistory[id] || [];
+			capHistory[id].push({
+				timestamp: rawCap.timestamp,
+				cap: rawCap.metrics.marketCap,
+			});
+		});
+
 		data.forEach(function(newSystemData) {
 			CurrentData.upsert({
 				name: newSystemData.name,
@@ -26,6 +53,7 @@ Meteor.startup(function() {
 					btc: parseFloat(newSystemData.metrics.price.btc) || 0,
 					usd: parseFloat(newSystemData.metrics.price.usd) || 0,
 				},
+				capHistory: capHistory[newSystemData.name + "/" + newSystemData.symbol],
 			}});
 		});
 	}));
