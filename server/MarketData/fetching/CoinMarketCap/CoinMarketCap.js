36051209f36282ea948f5a606eca59f69e44bdf6
Nikita Litvin <deltaidea@derpy.ru>
2015-04-30 22:52:22 +0500

    fix(fetching:CoinMarketCap): save trade volume as a number, not as a string
    This fixes sort order when trying to get a median value for the last month.

--- server/MarketData/fetching/CoinMarketCap/CoinMarketCap.js
+++ server/MarketData/fetching/CoinMarketCap/CoinMarketCap.js
@@ -25,6 +25,8 @@ this.fetching.coinMarketCap.processData = function(data, callback) {
 			metrics: _.omit(rawSystem, "position", "name", "symbol", "timestamp"),
 		};
 
+		processedSystem.metrics.volume24.btc = parseFloat(processedSystem.metrics.volume24.btc) || 0;
+
 		var tradeVolumeMedians = {
 			day: processing.getMedianValue("CoinMarketCap",
 				rawSystem, "metrics.volume24.btc", dayAgoTimestamp),
