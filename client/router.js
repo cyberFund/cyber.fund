fce663fecc16efce53927bbba2085eb5f6766bf7
Nikita Litvin <deltaidea@derpy.ru>
2015-05-09 02:54:44 +0500

    feat(client): use `core-animated-pages` for navigation between tabs

--- client/router.js
+++ client/router.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 	var menu = document.querySelector("cf-main-menu");
-	var pages = document.querySelector("core-pages");
+	var pages = document.querySelector("core-animated-pages");
 
 	menu.addEventListener("core-select", function() {
-		console.log("page selected:", pages.selected);
 		pages.selected = menu.selected;
 	});
-	pages.selected = menu.selected = 0;
+	menu.selected = 0;
 });
