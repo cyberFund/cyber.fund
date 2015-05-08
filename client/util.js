80268126c36802317bc2024e29587b966d89a829
Nikita Litvin <deltaidea@derpy.ru>
2015-05-09 00:52:55 +0500

    feat(client:rating-table): make numbers readable: "1000000" -> "1,000,000"

---
+++ client/util.js
@@ -0,0 +1,12 @@
+// Transforms "There're 12345678 apples" to "There're 12,345,678 apples".
+//
+// https://www.polymer-project.org/0.5/docs/polymer/expressions.html#writing-global-filters
+// http://stackoverflow.com/a/12947816
+document.addEventListener("DOMContentLoaded", function() {
+	PolymerExpressions.prototype.readableNumbers = function(input) {
+		while (/(\d+)(\d{3})/.test(input.toString())) {
+			input = input.toString().replace(/(\d+)(\d{3})/, "$1" + "," + "$2");
+		}
+		return input;
+	};
+});
