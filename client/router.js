c064e01c941148d9f13a9b64fbc8da7736927687
Nikita Litvin <deltaidea@derpy.ru>
2015-05-20 21:34:22 +0500

    feat(client): use `app-router` instead of `core-animated-pages`
    This means that pages are now loaded lazily.
    For optimization reasons animations are also now disabled.
    
    Also, the paper-tabs in the top menu now include `<a is="pushstate-anchor">`.
    
    This finally closes #51.

--- client/router.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("polymer-ready", function() {
-	var menu = document.querySelector("cf-main-menu");
-	var pages = document.querySelector("core-animated-pages");
-
-	menu.addEventListener("core-select", function() {
-		pages.selected = menu.selected;
-	});
-});
