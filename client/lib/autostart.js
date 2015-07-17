0c231a60792e56c14729c95f550291bb479982e7
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-07-17 14:18:47 +0600

    trying to fix "page" events

--- client/lib/autostart.js
+++ client/lib/autostart.js
@@ -1,3 +1,12 @@
 Tracker.autorun(function(){
 	Meteor.subscribe('userDetails');
 });
+
+if (Package['iron:router']) {
+	console.log("meow");
+	Package['iron:router'].Router.onRun(function() {
+		var router = this;
+		Tracker.afterFlush(function () { analytics.page(router.route.getName()); });
+		this.next();
+	});
+}
