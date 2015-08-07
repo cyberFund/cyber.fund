e993bf5540b6d9a0c0a1a98e9a0eca2ac5f87418
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-08-07 22:02:18 +0300

    supply + cap for etherium

--- server/fetching/etherium/etherium.js
+++ server/fetching/etherium/etherium.js
@@ -1,3 +1,33 @@
 /**
  * Created by angelo on 8/7/15.
  */
+var fetch = function(){
+  try {
+    var res = HTTP.get("https://etherchain.org/api/basic_stats");
+    if (!res.data) return;
+    var data = res.data;
+    if (data.status ==  1) {
+      data = data.data;
+      var blockCount = data.blockCount ? data.blockCount.number : data.difficulty ? data.difficulty.number : 0;
+      if (blockCount) {
+        var selector = {system: "Ethereum"};
+        var metrics = CurrentData.findOne(selector);
+        if (metrics.metrics) {
+          metrics = metrics.metrics;
+          var supply = blockCount*5 + 72002454.768;
+          var set = {
+            'metrics.su[pply': supply,
+            'metrics.cap.btc': metrics.price.btc *supply,
+            'metrics.cap.usd': metrics.price.usd *supply
+          };
+          CurrentData.update(selector, {$set: set});
+        }
+      }
+    } else  {
+      console.log("received status " +data.status + " when fetched etherium basic stats. no update.")
+    }
+  } catch (e) {
+    console.log("cannot fetch etherium data");
+    return;
+  }
+};
