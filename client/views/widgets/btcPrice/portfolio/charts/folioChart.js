7feaee757a4c655ad8b4418e61f0c84128707723
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-09-02 12:54:07 +0300

    initial pie chart is here

--- client/views/widgets/btcPrice/portfolio/charts/folioChart.js
+++ client/views/widgets/btcPrice/portfolio/charts/folioChart.js
@@ -1,15 +1,66 @@
+var sortFunction = function (x, y) {
+  if (!x.token || !x.token.token_symbol) return 1;
+  if (!y.token || !y.token.token_symbol) return -1;
+
+  var p1 =
+    CF.UserAssets.getQuantitiesFromAccountsObject(
+      CF.UserAssets.getAccountsObject(), x.token.token_symbol) * x.metrics.price.btc
+  var p2 =
+    CF.UserAssets.getQuantitiesFromAccountsObject(
+      CF.UserAssets.getAccountsObject(), y.token.token_symbol) * y.metrics.price.btc
+  return Math.sign(p2 > p1);
+}
+
 Template['folioChart'].rendered = function () {
-  
+
+  var self = this;
+  Tracker.autorun(function (comp) {
+    var ticks = [], labels = []
+
+    var options = Session.get("portfolioOptions") || {},
+      user = Meteor.user(),
+      symbols = CF.UserAssets.getSymbolsFromAccountsObject(CF.UserAssets.getAccountsObject());
+    var r = CurrentData.find(CF.CurrentData.selectors.symbol(symbols));
+    var data = r.fetch().sort(sortFunction);
+
+    console.log(data);
+    _.each(data, function (system) {
+      var q = CF.UserAssets.getQuantitiesFromAccountsObject(CF.UserAssets.getAccountsObject(), system.token.token_symbol);
+      var b = q * system.metrics.price.btc;
+      var u = q * system.metrics.price.usd;
+      labels.push (system.token.token_symbol)
+      ticks.push({
+        value: u,
+        meta: 'N: ' + q.toFixed(4) + '; BTC: ' + b.toFixed(4) + '; USD: ' + u.toFixed(2)
+      })
+    });
+
+    new Chartist.Pie('.ct-chart.folio-pie', {
+      labels: labels,
+      series: ticks
+    }, {
+      donut: true,
+      donutWidth: 90,
+      startAngle: 15,
+      //total: 200,
+      //showLabel: true,
+      plugins: [
+        Chartist.plugins.tooltip()
+      ]
+    });
+    //}
+    //comp.stop()
+  })
 };
 
 Template['folioChart'].helpers({
   'foo': function () {
-    
+
   }
 });
 
 Template['folioChart'].events({
   'click .bar': function (e, t) {
-    
+
   }
 });
--- client/views/widgets/btcPrice/portfolio/portfolioWidget.js
+++ client/views/widgets/btcPrice/portfolio/portfolioWidget.js
@@ -1,4 +1,6 @@
-function getAccountsObject() {
+
+// this only works for current user.
+CF.UserAssets.getAccountsObject = function() {
   var user = Meteor.user(),
     options = Session.get("portfolioOptions");//todo: factor out
   if (!user) return {};
@@ -15,16 +17,37 @@ Template['portfolioWidget'].rendered = function () {
   this.subscribe('portfolioSystems', Session.get('portfolioOptions'))
 };
 
+
+// sort portfolio items by their cost, from higher to lower.
+// return -1 if x > y; return 1 if y > x
+var sortFunction = function (x, y) {
+  if (!x.token || !x.token.token_symbol) return 1;
+  if (!y.token || !y.token.token_symbol) return -1;
+
+  var p1 =
+    CF.UserAssets.getQuantitiesFromAccountsObject(
+      CF.UserAssets.getAccountsObject(), x.token.token_symbol) * x.metrics.price.btc
+  var p2 =
+    CF.UserAssets.getQuantitiesFromAccountsObject(
+      CF.UserAssets.getAccountsObject(), y.token.token_symbol) * y.metrics.price.btc
+  return Math.sign(p2 > p1);
+}
+
 Template['portfolioWidget'].helpers({
-  'pSystems': function () {
+  'gSystems': function () { //  systems to display in portfolio chart, only present in user assets
     var options = Session.get("portfolioOptions") || {},
       user = Meteor.user(),
-      symbols = CF.UserAssets.getSymbolsFromAccountsObject(getAccountsObject())
-
+      symbols = CF.UserAssets.getSymbolsFromAccountsObject(CF.UserAssets.getAccountsObject());
+    var  r = CurrentData.find(CF.CurrentData.selectors.symbol(symbols));
+    return r.fetch().sort(sortFunction);
+  },
+  'pSystems': function () { //  systems to display in portfolio table, including 'starred' systems
+    var options = Session.get("portfolioOptions") || {},
+      user = Meteor.user(),
+      symbols = CF.UserAssets.getSymbolsFromAccountsObject(CF.UserAssets.getAccountsObject())
 
     var stars = user.profile.starredSystems;
     if (stars && stars.length) {
-
       var plck = _.map(CurrentData.find({system: {$in: stars}},
         {fields: {'token.token_symbol': 1}}).fetch(), function (it) {
         return it.token && it.token.token_symbol;
@@ -32,29 +55,13 @@ Template['portfolioWidget'].helpers({
       symbols = _.union(symbols, plck)
     }
     var r = CurrentData.find(CF.CurrentData.selectors.symbol(symbols));
-
-    // return -1 if x > y; return 1 if y > x
-    var sortFunction = function (x, y) {
-      if (!x.token || !x.token.token_symbol) return 1;
-      if (!y.token || !y.token.token_symbol) return -1;
-
-
-      var p1 =
-      CF.UserAssets.getQuantitiesFromAccountsObject(
-        getAccountsObject(), x.token.token_symbol) * x.metrics.price.btc
-      var p2 =
-        CF.UserAssets.getQuantitiesFromAccountsObject(
-          getAccountsObject(), y.token.token_symbol) * y.metrics.price.btc
-      return Math.sign(p2 > p1);
-    }
     return r.fetch().sort(sortFunction);
-
   },
   quantity: function (system) {
     if (!system.token || !system.token.token_symbol) return NaN;
 
     return CF.UserAssets.getQuantitiesFromAccountsObject(
-      getAccountsObject(), system.token.token_symbol);
+      CF.UserAssets.getAccountsObject(), system.token.token_symbol);
   }
   ,
   btcCost: function (system) {
@@ -62,19 +69,18 @@ Template['portfolioWidget'].helpers({
 
 
     if (system.metrics && (!system.metrics.price || !system.metrics.price.btc)) return "no btc price found..";
-    return CF.UserAssets.getQuantitiesFromAccountsObject(
-        getAccountsObject(), system.token.token_symbol) * system.metrics.price.btc;
+    return (CF.UserAssets.getQuantitiesFromAccountsObject(
+      CF.UserAssets.getAccountsObject(), system.token.token_symbol) * system.metrics.price.btc).toFixed(4);
   }
   ,
   usdCost: function (system) {
     if (!system.token || !system.token.token_symbol) return "no token for that system";
 
     if (system.metrics && (!system.metrics.price || !system.metrics.price.usd)) return "no usd price found..";
-    return CF.UserAssets.getQuantitiesFromAccountsObject(
-        getAccountsObject(), system.token.token_symbol) * system.metrics.price.usd;
+    return (CF.UserAssets.getQuantitiesFromAccountsObject(
+      CF.UserAssets.getAccountsObject(), system.token.token_symbol) * system.metrics.price.usd ).toFixed(2);
   }
-})
-;
+});
 
 Template['portfolioWidget'].events({
   'click .bar': function (e, t) {
