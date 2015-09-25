4e1cac4f06795d07b2d4b83dc0b363fb54788614
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-09-25 23:16:25 +0300

    project/radar cards iteration 5/100

--- client/views/widgets/radarCard/radarCard.js
+++ client/views/widgets/radarCard/radarCard.js
@@ -4,16 +4,10 @@ Template['radarCard'].rendered = function () {
 
 Template['radarCard'].helpers({
   'daysLeft': function (datestring, format) {
-    console.log(moment(datestring, format));
-    console.log(datestring);
-    console.log(moment());
-    console.log();
-    console.log();
-    console.log();
     return  moment(datestring, format).diff( moment() , 'days');
   },
   daysPassed: function (datestring, format) {
-
+    return  moment().diff(moment(datestring, format), 'days');
   },
   name_: function () {
     return Blaze._globalHelpers._toU(this.system);
