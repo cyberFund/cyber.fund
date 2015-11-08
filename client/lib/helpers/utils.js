70dc068fc3eeff2f0b42391da78e6c5018f0c40d
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-11-08 17:48:16 +0600

    merged from develop

---
+++ client/lib/helpers/utils.js
@@ -0,0 +1,16 @@
+function isElementInViewport (el) { //stackoverflow
+
+  //special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+  );
+}
