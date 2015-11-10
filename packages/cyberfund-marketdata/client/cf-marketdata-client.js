d39709712d477ef1a9a4497155694a8ef9981e44
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-11-10 11:35:55 +0600

    bugfix

--- packages/cyberfund-marketdata/client/cf-marketdata-client.js
+++ packages/cyberfund-marketdata/client/cf-marketdata-client.js
@@ -1,3 +1,4 @@
+CF.MarketData = CF.MarketData || {};
 CF.MarketData.graphTime = new CF.Utils.SessionVariable("CF.MarketData.graphTime");
 Meteor.startup(function(){
   if (!Session.get('CF.MarketData.graphTime')) CF.MarketData.graphTime.set("all");
