2b7472852a75fe4de94da47b0359b94cfbdb99a5
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-11-11 21:54:26 +0600

    more data calculated at invest

--- client/views/pages/invest.js
+++ client/views/pages/invest.js
@@ -28,13 +28,22 @@ Template['invest'].helpers({
   'left': function(){
     return this.invest ? 42 - _raised(this.invest) : 0
   },
-  'cap': function(){
+  'invcap': function(){
     return this.invest ? 100/3 * _raised(this.invest) : 0
   },
+  cap_btc_mln: function(){
+    return this.cap.btc/1000000
+  },
+  cap_usd_bln: function(){
+    return this.cap.usd/1000000000
+  },
+  cap_btc_div_200k: function(){
+    return this.cap.btc/200000
+  },
   'nicheShare': function(){
-    var share = (this.invest && this.cap) ? (100/ 3 *_raised(this.invest)) / this.cap.btc / 200: 0;
+    var share = (this.invest && this.cap) ? (100/ 3 *_raised(this.invest)) / (this.cap.btc / 200): 0;
     if (share == 0) return 0;
-    var rev = Math.round(1/share/10000)*10000;
+    var rev = Math.round(1/share);
 
     return "1/"+rev
   },
