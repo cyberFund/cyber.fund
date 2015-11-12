b66cb013a8475351ed304949217fe152d8cbea55
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-11-12 13:55:44 +0600

    reverted

--- packages/cyberfund-base/server/assetsCryptoBalance.js
+++ packages/cyberfund-base/server/assetsCryptoBalance.js
@@ -15,7 +15,7 @@ Meteor.methods({
    currently let s just
    */
   "getInvestData": function () {
-    var dasAddress = "3FvBgYaZV9347L863L4sQx75R6FQDReZKR";
+    var dasAddress = "19w5VB8cPZmQg4VqiNMWqo8mZRNVAC6PxG";
     var result = HTTP.get("https://blockchain.info/rawaddr/" + dasAddress);
     try {
       var ret = JSON.parse(result.content);
