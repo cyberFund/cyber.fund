c0508dfec7969f0bba091df6099bd4ad9db5d2b7
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-11-27 17:41:16 +0600

    radar LOADING...

--- client/views/pages/radarPage.js
+++ client/views/pages/radarPage.js
@@ -12,48 +12,51 @@ function _project() {
   return CurrentData.find(CF.Chaingear.selector.project);
 }
 
-Template['radarPage'].helpers({
-  crowdsale: function() {
-    return _crowdsale();
-  },
-  crowdsalePast: function() {
-    return CurrentData.find({
-      $and: [CF.Chaingear.selector.crowdsales, {
-        'crowdsales.end_date': {
-          $lt: new Date()
-        }
-      }]
-    }, {
-      sort: {
-        'crowdsales.end_date': -1
+function _crowdsalePast() {
+  return CurrentData.find({
+    $and: [CF.Chaingear.selector.crowdsales, {
+      'crowdsales.end_date': {
+        $lt: new Date()
       }
-    })
-  },
-  crowdsaleUpcoming: function() {
-    return CurrentData.find({
-      $and: [CF.Chaingear.selector.crowdsales, {
-        'crowdsales.start_date': {
-          $gt: new Date()
-        }
-      }]
-    })
-  },
-  crowdsaleActive: function() {
-    return CurrentData.find({
-      $and: [CF.Chaingear.selector.crowdsales, {
-        'crowdsales.end_date': {
-          $gt: new Date()
-        }
-      }, {
-        'crowdsales.start_date': {
-          $lt: new Date()
-        }
-      }]
-    })
-  },
-  project: function() {
-    return CurrentData.find(CF.Chaingear.selector.projects)
-  },
+    }]
+  }, {
+    sort: {
+      'crowdsales.end_date': -1
+    }
+  })
+}
+
+function _crowdsaleUpcoming() {
+  return CurrentData.find({
+    $and: [CF.Chaingear.selector.crowdsales, {
+      'crowdsales.start_date': {
+        $gt: new Date()
+      }
+    }]
+  })
+}
+
+function _crowdsaleActive() {
+  return CurrentData.find({
+    $and: [CF.Chaingear.selector.crowdsales, {
+      'crowdsales.end_date': {
+        $gt: new Date()
+      }
+    }, {
+      'crowdsales.start_date': {
+        $lt: new Date()
+      }
+    }]
+  })
+}
+
+Template['radarPage'].helpers({
+  crowdsale: function() { return _crowdsale() },
+  crowdsalePast: function() { return _crowdsalePast() },
+  crowdsaleUpcoming: function() { return _crowdsaleUpcoming() },
+  crowdsaleActive: function() { return _crowdsaleActive(); },
+  project: function() { return _project() },
+  nothing: function() { return !( _project().count() + _crowdsale().count() ) },
 
   isActiveCrowdsale: function() {
     return this.crowdsales && this.crowdsales.start_date < new Date() &&
