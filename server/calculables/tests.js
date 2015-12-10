f26e3f9fa78bb919f045706544609b5c2b64fde6
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-12-11 05:41:42 +0600

    client side disorder

--- server/calculables/tests.js
+++ server/calculables/tests.js
@@ -198,6 +198,10 @@ var weightsCS = {
 }
 
 Meteor.startup(function() {
+  if (Meteor.settings.byPassStartupCalculations) {
+    console.log ("initial calculations of CurrentData.calculables were disabled from meteor settings");
+    return;
+  }
   CF.CurrentData.calculatables.triggerCalc('firstDatePrice');
   CF.CurrentData.calculatables.triggerCalc('nLinksWithTag');
   //CF.CurrentData.calculatables.triggerCalc('CS');
