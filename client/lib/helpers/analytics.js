f7e0e4d6a44783db6ee73ec9bca14661f02bd175
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-12-29 18:18:20 +0600

    bugfix

--- client/lib/helpers/analytics.js
+++ client/lib/helpers/analytics.js
@@ -1,6 +1,6 @@
 var helpers = {
   segmentIoPublicKey: function() {
-    return Meteor.settings &&
+    return Meteor.settings
     && Meteor.settings.public
     && Meteor.settings.public["Segment.io"]
     && Meteor.settings.public["Segment.io"].apiKey
