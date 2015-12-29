617d8574678767ded37f196bb421b50ce466cc4f
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-12-29 17:40:25 +0600

    some dev stuff around dev server

---
+++ client/lib/helpers/analytics.js
@@ -0,0 +1,13 @@
+var helpers = {
+  segmentIoPublicKey: function() {
+    return Meteor.settings &&
+    && Meteor.settings.public
+    && Meteor.settings.public["Segment.io"]
+    && Meteor.settings.public["Segment.io"].apiKey
+    || "not found";
+  }
+};
+
+_.each(helpers, function (helper, key) {
+  Template.registerHelper(key, helper);
+});
