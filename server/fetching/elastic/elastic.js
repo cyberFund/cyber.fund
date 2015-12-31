88865a65653810b2db31dc7ff0d0bcfe3a3424b3
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2015-12-31 13:36:13 +0600

    refactored latest_values query, added server-side reporting of query fetch time

--- server/fetching/elastic/elastic.js
+++ server/fetching/elastic/elastic.js
@@ -70,56 +70,35 @@ var esParsers = {
     var notFounds = [];
     _.each(buckets, function(bucket) {
 
-      // elasticsearch returns 'date range' buckets in custom order,
-      // not corresponding to order they were defined. it looks depending on sort order by timestamp, used inside
-      // inner aggregations, so best is to explicitly get buckets.
-      // here we use bucket with max 'to' value as current, next bucket is 'day ago' bucket.. same as defined in aggregation..
-      var index = _.map(bucket.by_time.buckets, function(item) {
-        return item.to;
-      });
+      var set = {}, // changes object, to be used within doc update
+        sNow = {}, // current day
+        sDayAgo = {}, // past day data
+        sWeekAgo = {}; // not used so far
 
-      // current
-      var mx = _.max(index);
-      var m = moment.utc(mx);
-      var stamp = {
+      var current = bucket.now;
+
+      if (current.latest && current.latest.hits &&
+        _.isArray(current.latest.hits.hits) &&
+        current.latest.hits.hits.length > 0) {
+        sNow = current.latest.hits.hits[0]._source;
+      }
+      if (_.isEmpty(sNow)) return; // no need to update if no new data
+
+      var m = moment(sNow.timestampm);
+      var timestamp = m._d;
+
+      var stamp = { // no need after switching from chartist to highcharts..
         day: m.date(),
         hour: m.hours(),
         minute: m.minutes()
       };
 
-      //
-      var timestamp = m._d;
-      var current = _.find(bucket.by_time.buckets, function(item) {
-        return item.to == mx;
-      });
+      var dayAgo = bucket.yesterday;
 
-      // day ago
-      index = _.without(index, mx);
-      mx = _.max(index);
-      var dayAgo = _.find(bucket.by_time.buckets, function(item) {
-        return item.to == mx;
-      });
-
-      //
-      // updating CurrentData (metrics, mainly)
-      //
-      var set = {}, // changes object, to be used within doc update
-        sNow = {}, // current day
-        sDayAgo = {}, // past day data
-        sWeekAgo = {}; // not used so far
-      // 0.
-      // calculating sNow, sDayAgo
-      //
-
-      if (_.isArray(current.latest.hits.hits) && current.latest.hits.hits.length > 0) {
-        sNow = current.latest.hits.hits[0]._source;
-      }
-
-      if (_.isArray(dayAgo.latest.hits.hits) && dayAgo.latest.hits.hits.length > 0) {
+      if (dayAgo.latest && dayAgo.latest.hits &&
+        _.isArray(dayAgo.latest.hits.hits) && dayAgo.latest.hits.hits.length > 0) {
         sDayAgo = dayAgo.latest.hits.hits[0]._source;
       }
-
-      if (_.isEmpty(sNow)) return; // no need to update if no new data
 
       // current document, so we can take some values if none in fetched data
       var curDoc = CurrentData.findOne(_searchSelector(bucket.key), {
@@ -420,13 +399,15 @@ var esParsers = {
     });
   }
 };
-
+Meteor.startup(function(){
+  fetchLatest ({systems: gatherSymSys({})})
+})
 function fetchLatest(params) {
   try {
     var d = moment();
     var result = CF.Utils.extractFromPromise(CF.ES.sendQuery("latest_values", params));
     var n = moment();
-    console.log(" received response to query 'latest_values' after " + n.diff(d) );
+    console.log(" received response to query 'latest_values' after "+ n.diff(d, "milliseconds")+" milliseconds" );
     esParsers.latest_values(result)
   } catch (e) {
     logger.warn("could not fetch latest values");
@@ -440,7 +421,7 @@ function fetchAverage15m(params) {
     var d = moment();
     var result = CF.Utils.extractFromPromise(CF.ES.sendQuery("averages_last_15m", params));
     var n = moment();
-    console.log(" received response to query 'averages_last_15m' after " + n.diff(d) );
+    console.log(" received response to query 'averages_last_15m' after "+ n.diff(d, "milliseconds")+" milliseconds");
     esParsers.averages_l15(result);
   } catch (e) {
     logger.warn("could not fetch latest_!5m_averages");
@@ -453,7 +434,7 @@ function fetchAverages(params) {
   var d = moment();
   var result = CF.Utils.extractFromPromise(CF.ES.sendQuery("average_values_date_histogram", params));
   var n = moment();
-  console.log(" received response to query 'average_values_date_histogram' after " + n.diff(d) );
+  console.log(" received response to query 'average_values_date_histogram' after "+ n.diff(d, "milliseconds")+" milliseconds" );
   esParsers.averages_date_hist(result);
 }
 
