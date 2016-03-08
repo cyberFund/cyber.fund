27acc9474703d57df386e1f57a9780d3f1766c7d
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-03-08 03:29:47 +0200

    moving files

--- client/lib/analytics.js
+++ client/lib/analytics.js
@@ -1,36 +1,62 @@
+CF.Utils = CF.Utils || {};
+var ns = CF.Utils
 
- CF.Utils = CF.Utils || {};
- var ns = CF.Utils
+ns.segmentIoPublicKey = function() {
+  try {
+    return Meteor.settings.public.analyticsSettings["Segment.io"].apiKey;
+  } catch (e) {
+    return "not found";
+  }
+}
 
- ns.segmentIoPublicKey = function() {
-   try {
-     return Meteor.settings.public.analyticsSettings["Segment.io"].apiKey;
-   } catch (e) {
-     return "not found";
-   }
- }
+! function() {
+  var analytics = window.analytics = window.analytics || [];
+  if (!analytics.initialize)
+    if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice.");
+    else {
+      analytics.invoked = !0;
+      analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "group", "track", "ready", "alias", "page", "once", "off", "on"];
+      analytics.factory = function(t) {
+        return function() {
+          var e = Array.prototype.slice.call(arguments);
+          e.unshift(t);
+          analytics.push(e);
+          return analytics
+        }
+      };
+      for (var t = 0; t < analytics.methods.length; t++) {
+        var e = analytics.methods[t];
+        analytics[e] = analytics.factory(e)
+      }
+      analytics.load = function(t) {
+        var e = document.createElement("script");
+        e.type = "text/javascript";
+        e.async = !0;
+        e.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.com/analytics.js/v1/" + t + "/analytics.min.js";
+        var n = document.getElementsByTagName("script")[0];
+        n.parentNode.insertBefore(e, n)
+      };
+      analytics.SNIPPET_VERSION = "3.0.1";
+      analytics.load(CF.Utils.segmentIoPublicKey());
 
- !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
-     analytics.load(CF.Utils.segmentIoPublicKey());
-
-     // # debug clause.
-     // console.log(CF.Utils.segmentIoPublicKey());
- }}();
+      // # debug clause.
+      // console.log(CF.Utils.segmentIoPublicKey());
+    }
+}();
 
 
 Template.body.events({
-  'click a[target="_blank"]': function (e, t) {
+  'click a[target="_blank"]': function(e, t) {
     if (analytics) {
-      analytics.track("Clicked External Link",
-        {
-          link: $(e.currentTarget).attr('href')
-        })
+      analytics.track("Clicked External Link", {
+        link: $(e.currentTarget).attr('href')
+      })
     }
   }
 });
 
-Meteor.startup(function () {
-  Tracker.autorun(function (c) {
+Meteor.startup(function() {
+  Tracker.autorun(function(c) {
     var user = Meteor.user();
     if (!user) {
       return;
--- client/lib/autostart.js
+++ client/lib/autostart.js
@@ -1,8 +1,7 @@
 BlazeLayout.setRoot('body');
 
 Tracker.autorun(function () {
-  Meteor.subscribe('userDetails');
-  Meteor.subscribe("BitcoinPrice")
+
   if (Meteor.user()) {
     Meteor.call("getUserNumber", function (err, ret) {
       Session.set("userRegistracionCount", ret)
@@ -11,17 +10,8 @@ Tracker.autorun(function () {
 });
 
 Meteor.startup(function () {
+  Meteor.subscribe('userDetails');
+  Meteor.subscribe("BitcoinPrice");
   Meteor.subscribe('usersCount');
   Meteor.subscribe('coinsCount');
 });
-
-// track pages
-if (Package['iron:router']) {
-  Package['iron:router'].Router.onRun(function () {
-    var router = this;
-    Tracker.afterFlush(function () {
-        analytics.page(router.route.getName());
-    });
-    this.next();
-  });
-}
