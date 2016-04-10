09d80bb5fcd7a2bd15e68cb69331d3ac223193dc
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-04-10 13:04:58 +0300

    bugfix

--- client/lib/lib/submodules/758.js
+++ client/lib/lib/submodules/758.js
@@ -12,7 +12,6 @@ var logger = {
     if (typeof label === 'string') {
       if (typeof value === 'object') console.log(label + ": ", value);
       else console.log([label, value].join(": "));
-        );
     }
     if (!condensed) console.log();
   }
