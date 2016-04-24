99103ab2dab0a736a89d7224d284c3e76230efb6
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-04-24 16:02:46 +0200

    rewrite getUserCount method

--- server/lib/usersCounter.js
+++ server/lib/usersCounter.js
@@ -4,13 +4,9 @@
 Meteor.methods({
   getUserNumber: function(){
     var idx=null;
-    var uid = this.userId;
     if (!this.userId) return(idx);
-    var users = Meteor.users.find({}, {sort: {createdAt: 1}})
-
-    users.forEach(function(doc, index, cursor){
-      if (doc._id == uid) idx = index+1;
-    })
+    var user = Meteor.users.findOne({_id: this.userId});
+    idx = user && user.registerNumber || null;
     return idx
   }
 })
