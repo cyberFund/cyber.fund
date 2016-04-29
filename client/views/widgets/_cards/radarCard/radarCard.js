70af8368081d069974075ecb3938ea0ee4f79cc3
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-04-29 12:08:03 +0200

    added stars to radar cards

--- client/views/widgets/_cards/radarCard/radarCard.js
+++ client/views/widgets/_cards/radarCard/radarCard.js
@@ -3,6 +3,7 @@ Template['radarCard'].rendered = function () {
 };
 
 Template['radarCard'].helpers({
+
   name_: function () { //TODO: move to global helpers ?
     return Blaze._globalHelpers._toUnderscores(this._id);
   },
