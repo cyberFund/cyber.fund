e1b973c1413962a2561d1101d4ba567d2038a739
Dima Starodubcev <d@cyber.fund>
2016-04-30 12:45:04 +0100

    Update crowdsales funds every 10 minutes

--- server/fetching/cyberFund/crowdsales.js
+++ server/fetching/cyberFund/crowdsales.js
@@ -83,7 +83,7 @@ SyncedCron.add({
   name: 'update active crowdsales',
   schedule: function(parser) {
     // parser is a later.parse object
-    return parser.text('every 40 minutes');
+    return parser.text('every 10 minutes');
   },
   job: function() {
     updateCrowdsales()
