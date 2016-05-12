88296eb0884f4e83591d57fafbcd7dab361296a0
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-05-12 19:14:01 +0200

    fixing chart

--- server/chartizm/methods.js
+++ server/chartizm/methods.js
@@ -1,3 +1,4 @@
+var day = 1000 * 60 * 60 * 24;
 Meteor.methods({
   fetchMarketData1: function(systemId){
     return MarketData.find({
@@ -6,10 +7,19 @@ Meteor.methods({
       : "daily" }).fetch()
   },
   fetchMarketData2: function(systemId, from, to){
-    return MarketData.find({
+    var ret = MarketData.find({
       systemId: systemId,
       interval: {$in: ['daily', 'hourly']},
       timestamp: {$gte: from, $lte: to}
-    }).fetch()
-  },
+    }).fetch();
+
+    if ((to.valueOf() - from.valueOf())/day < 4) {
+      ret = ret.concat(FastData.find({
+        systemId: systemId,
+        interval: {$in: ['daily', 'hourly']},
+        timestamp: {$gte: from, $lte: to}
+      })).fetch();
+    }
+    return ret;
+  }
 })
