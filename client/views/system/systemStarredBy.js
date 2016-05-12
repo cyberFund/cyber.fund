3a7c00cb449a7e3b949fecbe3a25579532ee8982
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-05-12 17:47:09 +0200

    missing files

---
+++ client/views/system/systemStarredBy.js
@@ -0,0 +1,21 @@
+function key(){
+  return 'showStarredBy';
+}
+
+Template['systemStarredBy'].onCreated(function() {
+  var instance = this;
+  var flag;
+  instance.autorun(function() {
+    if (Session.get(key()) || flag) {
+      var data = CurrentData.findOne({_id: Blaze._globalHelpers._toSpaces(FlowRouter.getParam('name_'))})
+      if (data && Session.get('showStarredBy')) {
+        instance.subscribe('avatars', data._usersStarred);
+      }
+      flag = true
+    }
+  });
+});
+
+Template['systemStarredBy'].helpers({
+  key: key
+})
