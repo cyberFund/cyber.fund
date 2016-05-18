68762eb95ee711664ed4dc88daa23791fbbd5f3f
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-05-18 19:14:29 +0200

    beta gone

--- client/views/profile/lib/helpers.js
+++ client/views/profile/lib/helpers.js
@@ -1,3 +1,12 @@
+CF.Profile.currentUid = function() {
+  var u = CF.User.findOneByUsername(CF.Profile.currentUsername());
+  return u ? u._id : null;
+};
+
+CF.Profile.currentUsername = function(){
+  return FlowRouter.getParam("username");
+};
+
 var helpers = ({
   isOwnAssets: function(){
     return CF.Profile.currentUid() == Meteor.userId();
