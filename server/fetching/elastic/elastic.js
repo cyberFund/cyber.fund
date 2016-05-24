c3e2b3ef5b7c745b3a7e2181a922f78b579e8b87
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-05-24 13:33:52 +0200

    trying app-root-path

--- server/fetching/elastic/elastic.js
+++ server/fetching/elastic/elastic.js
@@ -36,30 +36,10 @@ function _searchSelector(bucketKey) {
 // data array; function to handle single item; delay in ms.
 // suitable for small arrays, and when we re sure calls won't interfere one another
 // (i.e. call period > delay*array.length)
-function handleArrayWithInterval(array, delay, handler, handlerAfter){
-  if (delay) {
-    var current = 0;
-    var length = array.length;
-
-    var interval = Meteor.setInterval(function(){
-      if (current < length) {
-        var item = array[current];
-        handler(item);
-        ++current;
-      } else {
-        Meteor.clearInterval(interval);
-        if (handlerAfter) handlerAfter(array);
-      }
-    }, delay);
-  } else { // no delay
-
-    _.each(array, function(item){
-      handler(item);
-    });
-    if (handlerAfter) handlerAfter(array);
-  }
-}
-
+const appRoot = require('app-root-path');
+const handleArrayWithInterval = require(appRoot + 
+  "/imports/api/handleArray").handleArrayWithInterval;
+console.log (handleArrayWithInterval, "oooooooooooooooooooooooooooooo")
 JSON.unflatten = function(data) {
   "use strict";
   if (Object(data) !== data || Array.isArray(data)) {
