85e79c459a1df9c0fa33594e10d43cc6d168eeb5
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-05-24 20:06:42 +0200

    bugfix (2/10)

--- client/views/funds/funds.js
+++ client/views/funds/funds.js
@@ -40,6 +40,7 @@ Template.funds.helpers({
   rowsIFollow: function(){
     const iFollow = fundsIFollow();
     let selector = require("../../../imports/userFunds/").selector
+    if (iFollow) selector._id = {$nin: iFollow}
     return Meteor.users.find(selector, {
       sort: {publicFunds: -1}
     });
