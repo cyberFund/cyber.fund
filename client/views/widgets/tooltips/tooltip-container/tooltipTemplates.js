2662998be63503fdccb0bcc885c04dc5122878ff
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-05-31 18:47:39 +0300

    quick & dirty monthly changes version of rating page - intermediate

--- client/views/widgets/tooltips/tooltip-container/tooltipTemplates.js
+++ client/views/widgets/tooltips/tooltip-container/tooltipTemplates.js
@@ -1,5 +1,5 @@
-Template['withTooltip'].events({
-  'mouseenter .with-tooltip': function(e, t) {
+Template["withTooltip"].events({
+  "mouseenter .with-tooltip": function(e, t) {
     if (!t.data || !t.data.ttName) return true;
 
     CF.tooltip.timer && (CF.tooltip.timer.hostInstance = t) &&
@@ -8,11 +8,11 @@ Template['withTooltip'].events({
         CF.tooltip.fire(function() {
           if (!CF.tooltip.instance) {
             CF.tooltip.instance =
-              Blaze.renderWithData(Template['tooltipContainer'], {
+              Blaze.renderWithData(Template["tooltipContainer"], {
                 ttName: t.data.ttName,
-                data: t.data.data,
+                data: _.extend(_.clone(t.data.data), {_period: t.data._period}),
                 class: t.data.class
-              }, t.$(".with-tooltip")[0])
+              }, t.$(".with-tooltip")[0]);
           }
         }, 300);
       });
@@ -20,21 +20,24 @@ Template['withTooltip'].events({
     return false;
   },
 
-  'mouseleave .with-tooltip': function(e, t) {
-    CF.tooltip.timer && CF.tooltip.timer.stop()
+  "mouseleave .with-tooltip": function(e, t) {
+    CF.tooltip.timer && CF.tooltip.timer.stop();
     if (CF.tooltip.instance) {
       Blaze.remove(CF.tooltip.instance);
       CF.tooltip.instance = null;
     }
   }
-})
+});
 
-Template['tooltipContainer'].helpers({
-  'tooltipStyleClass': function(kls) {
-    return CF.tooltip.styleClass(kls)
+Template["tooltipContainer"].helpers({
+  "tooltipStyleClass": function(kls) {
+    return CF.tooltip.styleClass(kls);
   },
-  getData: ()=> {
+  getData: function() {
     var ret = _.clone(this.data);
-    return _.extend(ret, {_period: this._period}) //dirty.
+    _.extend(ret, {_period: this._period}); //dirty.
+    console.log(ret);
+    console.log(this);
+    return ret;
   }
-})
+});
