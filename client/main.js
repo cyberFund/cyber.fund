ab14197faa69f4796f46bdf51865e211c5b889a9
Mihail Paley <undeadlol1@mail.ru>
2016-06-03 16:54:34 +0300

    adds hello world react project with basic routes

---
+++ client/main.js
@@ -0,0 +1,11 @@
+/*import React from 'react'
+import { Meteor } from 'meteor/meteor'
+import { render } from 'react-dom'
+import '../imports/startup/accounts-config'*/
+
+// fmdl's layout component does not work properly with react
+// thats why react-mdl is used https://github.com/tleunen/react-mdl#requirements
+import '../node_modules/react-mdl/extra/material.css'
+import '../node_modules/react-mdl/extra/material.js'
+
+import '../imports/startup/routes'
---
+++ imports/startup/routes.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { mount } from 'react-mounter'
+
+import MainLayout from '../ui/pages/MainLayout'
+import IndexPage from '../ui/pages/IndexPage'
+import RadarPage from '../ui/pages/RadarPage'
+import RatingPage from '../ui/pages/RatingPage'
+import FundsPage from '../ui/pages/FundsPage'
+
+const routes = [
+  ['/', <IndexPage />],
+  ['/radar', <RadarPage />],
+  ['/rating', <RatingPage />],
+  ['/funds', <FundsPage />]
+].forEach( item =>{
+    FlowRouter.route(item[0], {
+      action() {
+        mount(MainLayout, {
+          main: item[1]
+        })
+      }
+    })
+  })
+
+FlowRouter.notFound = {
+    action: function() {
+      console.warn('Route not found! Redirecting to index page')
+      mount(MainLayout, {
+        // TODO: create 404Page component
+          main: <IndexPage />
+      })
+    }
+}
