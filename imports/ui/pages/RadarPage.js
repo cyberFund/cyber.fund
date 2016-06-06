c3adac28a6deb791e6d0c316d9eda4b3acffec51
Mihail Paley <undeadlol1@mail.ru>
2016-06-06 11:04:07 +0300

    refactor RadarPage and fix responsiveness

--- imports/ui/pages/RadarPage.js
+++ imports/ui/pages/RadarPage.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
 import { Meteor } from 'meteor/meteor'
-import CrowdsalesList from '../components/CrowdsalesList'
+import CrowdsaleCardList from '../components/CrowdsaleCardList'
 import { Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton, Grid, Cell } from 'react-mdl'
 
 
@@ -9,64 +9,57 @@ class RadarPage extends Component {
   render() {
     const p = this.props
     // TODO: make proper loading indication
-    const loadingIndicator = (<h3 className="center">Scanning cyberspace</h3>)
+    // const loadingIndicator = (<h3 className="center">Scanning cyberspace</h3>)
 
-    return (// p.loaded ?
-  <div style={{width: '80%', margin: 'auto', textAlign: 'center'}}>
-    <Grid>
-      <Cell col={12}>
-        <Card shadow={1} style={{width: '512px', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>Welcome</CardTitle>
-            <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Mauris sagittis pellentesque lacus eleifend lacinia...
-            </CardText>
-            <CardActions border>
-                <Button colored>Get Started</Button>
-            </CardActions>
-            <CardMenu style={{color: '#fff'}}>
-                <IconButton name="stars" />
-            </CardMenu>
-        </Card>
-      </Cell>
-    </Grid>
-    <Grid>
-        <Cell col={12}>
-          <h2>Free Services For Ingenious Inventors</h2>
-          <p>If you excited about creating or already working on a blockchain project you have awesome free option to list your project in cyber•Fund's Radar.</p>
-        </Cell>
-    </Grid>
-    <Grid>
-        <Cell col={4} tablet={4} phone={12}>
-            <i style={{color: '#ffeb3b', fontSize: '6em'}} className="material-icons">school</i>
-            <h5>Due Diligence</h5>
-        </Cell>
-        <Cell col={4} tablet={4} phone={12}>
-            <i style={{color: '#cddc39', fontSize: '6em'}} className="material-icons">group</i>
-            <h5>Engaged Audience</h5>
-        </Cell>
-        <Cell col={4} tablet={12} phone={12}>
-            <i style={{color: '#e91e63', fontSize: '6em'}} className="material-icons">whatshot</i>
-            <h5>Blockchain Reporting</h5>
-        </Cell>
-    </Grid>
-    <Grid>
-      <Cell col={12}>
-        <a
-          className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          href="http://cybertalks.org/t/radar-listing-of-cryptocurrencies-and-cryptoassets/354"
-          target="blank">
-          Get Listed!
-        </a>
-      </Cell>
-    </Grid>
-  </div>
-    ) // : <Loading />
+    return (
+      <div>
+        {/* INFO SECTION */}
+        <div>
+          <CrowdsaleCardList title="Active Crowdsales" type="active" items={p.active} />
+          <CrowdsaleCardList title="Upcoming Crowdsales" type="upcoming" items={p.upcoming} />
+          <CrowdsaleCardList title="Anticipated Projects" type="projects" items={p.projects} />
+          <CrowdsaleCardList title="Successful Crowdsales" type="past" items={p.past} />
+        </div>
+        {/* CALL TO ACTION */}
+        <div className="text-center">
+          <Grid>
+              <Cell col={12}>
+                <h2>Free Services For Ingenious Inventors</h2>
+                <p>If you excited about creating or already working on a blockchain project you have awesome free option to list your project in cyber•Fund's Radar.</p>
+              </Cell>
+          </Grid>
+          <Grid>
+              <Cell col={4} tablet={4} phone={12}>
+                  <i style={{color: '#ffeb3b', fontSize: '6em'}} className="material-icons">school</i>
+                  <h5>Due Diligence</h5>
+              </Cell>
+              <Cell col={4} tablet={4} phone={12}>
+                  <i style={{color: '#cddc39', fontSize: '6em'}} className="material-icons">group</i>
+                  <h5>Engaged Audience</h5>
+              </Cell>
+              <Cell col={4} tablet={12} phone={12}>
+                  <i style={{color: '#e91e63', fontSize: '6em'}} className="material-icons">whatshot</i>
+                  <h5>Blockchain Reporting</h5>
+              </Cell>
+          </Grid>
+          <Grid>
+            <Cell col={12}>
+              <Button
+                raised accent ripple
+                component="a"
+                href="http://cybertalks.org/t/radar-listing-of-cryptocurrencies-and-cryptoassets/354"
+                target="blank">
+                Get Listed!
+              </Button>
+            </Cell>
+          </Grid>
+        </div>
+      </div>
+    )
   }
 }
 
 RadarPage.propTypes = {
- crowdsales: PropTypes.array.isRequired,
  projects: PropTypes.array.isRequired,
  past: PropTypes.array.isRequired,
  upcoming: PropTypes.array.isRequired,
@@ -76,58 +69,56 @@ RadarPage.propTypes = {
 export default createContainer(() => {
   Meteor.subscribe('crowdsalesAndProjectsList')
   const {selectors} = CF.CurrentData,
-  crowdsales = CurrentData.find(selectors.crowdsales()).fetch(),
   projects =  CurrentData.find({
-    $or: [{
-      'crowdsales.start_date': {
-        $gt: new Date()
-      }
+      $or: [{
+        'crowdsales.start_date': {
+          $gt: new Date()
+        }
+      }, {
+        'crowdsales.start_date': {
+          $exists: false
+        }
+      }],
+      $and: [
+      selectors.projects()]
     }, {
-      'crowdsales.start_date': {
-        $exists: false
+      sort: {
+        "calculatable.RATING.vector.LV.sum": -1
       }
-    }],
-    $and: [
-    selectors.projects()]
-  }, {
-    sort: {
-      "calculatable.RATING.vector.LV.sum": -1
-    }
-  }).fetch(),
+    }).fetch(),
   past = CurrentData.find({
-    $and: [selectors.crowdsales(), {
-      'crowdsales.end_date': {
-        $lt: new Date()
-      }
-    }]
-  }, {
-    sort: {
-      'crowdsales.end_date': -1
-    }
-  }).fetch(),
-  upcoming = CurrentData.find({
-    $and: [selectors.crowdsales(), {
-      'crowdsales.start_date': {
-        $gt: new Date()
-      }
-    }]
-  }, {
-    sort: {
-      'crowdsales.start_date': 1
-    }
-  }).fetch(),
-  active = CurrentData.find({
-    $and: [selectors.crowdsales(), {
-      'crowdsales.end_date': {
-        $gt: new Date()
-      }
+      $and: [selectors.crowdsales(), {
+        'crowdsales.end_date': {
+          $lt: new Date()
+        }
+      }]
     }, {
-      'crowdsales.start_date': {
-        $lt: new Date()
+      sort: {
+        'crowdsales.end_date': -1
       }
-    }]
-  }, {sort: {"metrics.currently_raised": -1}}).fetch()
-  console.log('everything is fine!')
-  console.log(crowdsales, projects, past, upcoming, active)
-  return {crowdsales, projects, past, upcoming, active}
+    }).fetch(),
+  upcoming = CurrentData.find({
+      $and: [selectors.crowdsales(), {
+        'crowdsales.start_date': {
+          $gt: new Date()
+        }
+      }]
+    }, {
+      sort: {
+        'crowdsales.start_date': 1
+      }
+    }).fetch(),
+  active = CurrentData.find({
+      $and: [selectors.crowdsales(), {
+        'crowdsales.end_date': {
+          $gt: new Date()
+        }
+      }, {
+        'crowdsales.start_date': {
+          $lt: new Date()
+        }
+      }]
+    }, {sort: {"metrics.currently_raised": -1}}).fetch()
+  // seems like we don't use all of these
+  return { projects, past, upcoming, active }
 }, RadarPage)
