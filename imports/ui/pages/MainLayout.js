7557deba3617af3e352a9b35cd9f1009d0d2e3d8
Mihail Paley <undeadlol1@mail.ru>
2016-06-07 12:11:49 +0300

    fix and refactor Header and Footer into MainLayout
    Long story short: header and footer will not be reused in near future,
    so they do not need to be a independent components.

--- imports/ui/pages/MainLayout.js
+++ imports/ui/pages/MainLayout.js
@@ -1,20 +1,140 @@
 import React from 'react'
-import { Layout, Content } from 'react-mdl'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import { Layout, Content,
+  Header, Navigation, Drawer, Textfield,
+  Footer, FooterSection, FooterDropDownSection, FooterLinkList } from 'react-mdl'
+
 
 class MainLayout extends React.Component {
- render() { return (
-   // render specified nav or footer or default one
-   <Layout>
-      {this.props.nav || <Header />}
-      {/* do we need to place <main> outside of
-       <Content /> for SEO purposes? */}
-      <Content>
-        {this.props.main}
-      </Content>
-      {this.props.footer || <Footer />}
-    </Layout>
+ render() {
+   const brandLink = <a
+                      style={{color: 'white'}}
+                      className="mdl-button mdl-js-button mdl-js-ripple-effect"
+                      href="/">
+                      Cyber.Fund
+                     </a>
+   return (
+     <Layout>
+
+       {/* HEADER NAV */}
+       <Header scroll title={brandLink}>
+           <Navigation>
+               <a href="/radar">Crowdsales</a>
+               <a href="/rating">Assets</a>
+               <a href="/funds">Funds</a>
+               <Textfield
+                   value=""
+                   onChange={() => {}}
+                   label="Search for coin"
+                   expandable
+                   expandableIcon="search"
+               />
+           </Navigation>
+       </Header>
+
+       {/* MOBILE SIDEBAR */}
+       <Drawer title={brandLink}>
+           <Navigation>
+               <a href="/radar">Crowdsales</a>
+               <a href="/rating">Assets</a>
+               <a href="/funds">Funds</a>
+               <Textfield
+                   value=""
+                   onChange={() => {}}
+                   label="Search for coin"
+                   expandable
+                   expandableIcon="search"
+               />
+           </Navigation>
+       </Drawer>
+
+       {/* MAIN CONTENT */}
+        <Content component="main">
+          {this.props.main}
+        </Content>
+
+        {/* FOOTER */}
+        <Footer size="mega">
+          <FooterSection type="middle">
+                  <h5>About
+                    cyber<span style={{color: 'yellow', fontSize: '0.85em'}}>
+                      <strong>•</strong>
+                    </span>Fund
+                  </h5>
+                  <p>Our mission to make digital investments comprehensible, accessible, easy and safe.</p>
+          </FooterSection>
+          <FooterSection type="middle">
+              <FooterDropDownSection title="Build">
+                  <FooterLinkList>
+                      <a
+                        href="http://cybertalks.org/t/radar-listing-of-cryptocurrencies-and-cryptoassets/354"
+                        target="_blank">
+                        Listing
+                      </a>
+                      <a
+                        href="https://github.com/cyberFund/chaingear"
+                        target="_blank">
+                        Chaingear
+                      </a>
+                      <a
+                        href="https://github.com/cyberFund"
+                        target="_blank">
+                        Github
+                      </a>
+                  </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="About">
+                  <FooterLinkList>
+                    <a href="/decisions">
+                      Decisions
+                    </a>
+                    <a
+                      href="https://www.coinprism.info/asset/AbhBFwWFpZzFMsBakZbDzUTYv67t9rThaK"
+                      target="_blank">
+                      Cryptoasset
+                    </a>
+                    <a
+                      href="https://www.coinprism.info/asset/AbhBFwWFpZzFMsBakZbDzUTYv67t9rThaK/owners"
+                      target="_blank">
+                      Holders
+                    </a>
+                  </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="Connect">
+                  <FooterLinkList>
+                      <a
+                        href="https://blog.cyber.fund/"
+                        target="_blank">
+                        Medium
+                      </a>
+                      <a
+                        href="https://twitter.com/cyberfundio"
+                        target="_blank">
+                        Twitter
+                      </a>
+                      <a
+                        href="https://gitter.im/orgs/cyberFund/rooms"
+                        target="_blank">
+                        Gitter
+                      </a>
+                  </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="Projects">
+                  <FooterLinkList>
+                      <a href="/system/SatoshiFund">Satoshi•Fund</a>
+                      <a href="/system/SatoshiPie">Satoshi•Pie</a>
+                      <a href="http://daobase.org" target="_blank">Daobase</a>
+                  </FooterLinkList>
+              </FooterDropDownSection>
+            </FooterSection>
+            <FooterSection type="bottom" logo="">
+                <FooterLinkList>
+                  <a href="https://mixpanel.com/f/partner">
+                    <img src="//cdn.mxpnl.com/site_media/images/partner/badge_light.png" alt="Mobile Analytics" />
+                  </a>
+                </FooterLinkList>
+            </FooterSection>
+        </Footer>
+      </Layout>
   )}
 }
 
