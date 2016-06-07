de023b2f9a570c229edf33b89c12ed0154251456
Mihail Paley <undeadlol1@mail.ru>
2016-06-07 18:05:42 +0300

    merge more helper functions
    cf-market-data meteor package > helpers.js module

--- imports/ui/helpers.js
+++ imports/ui/helpers.js
@@ -34,6 +34,8 @@ function escapeNaN(formatter){
 }
 
 const helpers = {
+  /* this part is originally from cyberfund-base package */
+
   eq: function
     (value1, value2) {
     return value1 === value2;
@@ -279,6 +281,67 @@ const helpers = {
   },
   isInDevelopmentMode: function isInDevelopmentMode(){
     return CF._mode;
+  },
+
+  /* this part is from cf-market-data */
+  dailyTradeVolumeToText: function (volumeDaily, absolute, needDigit) {
+    if (!absolute) {
+      return needDigit ? 0 : "Normal";
+    }
+
+    if (Math.abs(volumeDaily / absolute) === 0) return needDigit ? 0 : "Illiquid";
+    if (Math.abs(volumeDaily / absolute) < 0.0001) return needDigit ? 0.1 : "Very Low";
+    if (Math.abs(volumeDaily / absolute) < 0.001) return needDigit ? 0.2 : "Low";
+    if (Math.abs(volumeDaily / absolute) < 0.005) return needDigit ? 0.3 : "Normal";
+    if (Math.abs(volumeDaily / absolute) < 0.02) return needDigit ? 0.4 : "High";
+    return needDigit ? 0.5 : "Very High";
+  },
+  greenRedNumber: function (value) {
+    return (value < 0) ? "red-text" : "green-text";
+  },
+  inflationToText: function (percents) {
+    if (percents < 0) {
+      return "Deflation " + (-percents).toFixed(2) + "%";
+    } else if (percents > 0) {
+      return "Inflation " + percents.toFixed(2) + "%";
+    } else {
+      return "Stable";
+    }
+  },
+  percentsToTextUpDown: function (percents, precision) {
+    if (!precision) precision = 2;
+    if (precision == 100) precision = 0;
+
+    if (percents < 0) {
+      return "↓ " + (-percents.toFixed(precision)) + "%";
+    } else if (percents > 0) {
+      return "↑ " + percents.toFixed(precision) + "%";
+    } else {
+      return "= 0%";
+    }
+  },
+  dayToDayTradeVolumeChange: function(system) {
+    var metrics = system.metrics;
+    if (metrics.tradeVolumePrevious && metrics.tradeVolumePrevious.day) {
+      return CF.Utils.deltaPercents(metrics.tradeVolumePrevious.day, metrics.tradeVolume);
+    }
+    return 0;
+  },
+  monthToMonthTradeVolumeChange: function(system) {
+    var metrics = system.metrics;
+    if (metrics.tradeVolumePrevious && metrics.tradeVolumePrevious.month) {
+      return CF.Utils.deltaPercents(metrics.tradeVolumePrevious.month, metrics.tradeVolume);
+    }
+    return 0;
+  },
+  chartdata: function(systemId/*, interval:: oneof ['daily', 'hourly']*/) {
+    return MarketData.find({systemId: systemId});
+  },
+  chartdataOrdered: function(systemId/*, interval:: oneof ['daily', 'hourly']*/) {
+    return MarketData.find({systemId: systemId}, {sort: {timestamp: -1}});
+  },
+  chartdataSubscriptionFetch: function(systemId/*, interval:: oneof ['daily', 'hourly']*/) {
+    return MarketData.find({systemId: systemId}, {sort: {timestamp: -1}}). fetch();
   }
 }
 export default helpers
