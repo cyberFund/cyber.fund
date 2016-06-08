407e4f491ef685d29d60b4182273d97e7a36d830
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-06-08 10:31:37 +0300

    markets table

--- client/views/system/markets/testMarkets.js
+++ client/views/system/markets/testMarkets.js
@@ -16,10 +16,10 @@ CF.test.printPairsWeighted = function(){
   console.log(feedsVwapCurrent.find().fetch())
 }
 
-// get weighted native price, given that there exist both direct and reverted
-// market pairs.
 
-function getWeightedPriceNative (base, quote){
+// get weighted native price, given that there can exist both direct and
+// reverted market pairs.
+function weightedPriceNative (base, quote){
   if (base === quote) return 1;
   //1. get both direct and revert price
   // note: for non-weighted pairs - there d be find().fetch(), and also weighting/getting reverse price would involve extra mapreduce step
@@ -47,7 +47,27 @@ function getWeightedPriceNative (base, quote){
   }
 }
 
-CF.test.gwp = getWeightedPriceNative;
+
+// return name of currently picked fiat. as it s given by xchange
+// todo: move to imports
+function _fiat(){
+  const fiat = CF.Utils._session.get('fiat');
+  if (fiat==='') return 'Bitcoin';
+  return fiat;
+}
+
+// return token of currently picked fiat. see `fiatSelector` template
+// todo: move to imports
+function _fiatToken(){
+  const fiat = CF.Utils._session.get('fiat');
+  if (fiat==="") return `BTC`;
+  //todo exploit chg
+  if (fiat==="Euro") return 'EUR';
+  if (fiat==="US Dollar") return 'USD';
+}
+
+
+CF.test.gwp = weightedPriceNative;
 Template["testMarkets"].onCreated(function(){
   const system = Template.currentData().system;
 
@@ -66,9 +86,16 @@ Template['testMarkets'].helpers({
     const system = this.system;
     const selector = selectors.pairsById
     const count = feedsCurrent.find(selector).count();
-    return feedsCurrent.find(selector, {
-      sort: {volume: -1},
-      limit: (count <= ROWS_SHORT || Template.instance().showAll.get()) ? 1000 : ROWS_SHORT})
+    function sortVolumeBtc(x, y){
+      function volumeBtc(point){
+        return point.volume.native/weightedPriceNative(point.quote, "Bitcoin");
+      }
+      return Math.sign(volumeBtc(y) - volumeBtc(x))
+    }
+    let ret = feedsCurrent.find(selector).fetch().sort(sortVolumeBtc);
+    console.log(ret);
+    if (!Template.instance().showAll.get()) ret = ret.slice(0, ROWS_SHORT);
+    return ret;
   },
   marketUrlByApiUrl: (apiUrl) => {
     const market = markets[apiUrl];
@@ -82,23 +109,26 @@ Template['testMarkets'].helpers({
     const sys = CurrentData.findOne({_id: _id}, {fields: {token: 1}});
     return sys && sys.token && sys.token.symbol || _id
   },
+  // not used
   pricePair: function() {
     return this.last && this.last.native
   },
+  // not used
   pricePairBtc: function() {
     const native = this.last && this.last.native;
 
     // A/btc = A/B * B/btc
-    return native / getWeightedPriceNative(this.base, "Bitcoin")
+    return native / weightedPriceNative(this.base, "Bitcoin")
   },
+  fiatToken: _fiatToken,
   pricePairFiat: function(){
     const native = this.last && this.last.native;
-    const fiat = CF.Utils._session.get('fiat') //todo => ../imports
-    let ret = native / getWeightedPriceNative(this.base, "Bitcoin");
-    if (fiat) ret = ret / getWeightedPriceNative("Bitcoin", fiat);
-    return ret;
+    const ret = native / weightedPriceNative(this.base, "Bitcoin");
+    return ret / weightedPriceNative("Bitcoin", _fiat());
   },
-  volumePair: function() {
-    return this.volume && this.volume.native
+  volumePairFiat: function() {
+    const native = this.volume && this.volume.native;
+    return this.volume.native/weightedPriceNative(this.quote, "Bitcoin")/
+    weightedPriceNative("Bitcoin", _fiat());
   }
 })
