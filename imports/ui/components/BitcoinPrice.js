c36695688c14a2bd0507f962a6ad28eaad2492be
Mihail Paley <undeadlol1@mail.ru>
2016-06-09 15:40:23 +0300

    fix <BitcoinPrice />

--- imports/ui/components/BitcoinPrice.js
+++ imports/ui/components/BitcoinPrice.js
@@ -1,56 +1,24 @@
 import React from 'react'
+import { If, Then, Else } from 'react-if'
+import Loading from "./Loading"
+import helpers from '../helpers'
 
-className Bitcoin extends React.Component {
+class BitcoinPrice extends React.Component {
   constructor(params){
     super(params)
+    const data = helpers._btcPrice()
     this.state = {
-      lastAddress: undefined,
-      lastStatus: undefined
+      btcPrice: data ? helpers.readableNumbers(data) : ''
     }
   }
   render () {
-    return (
-      <div className="card">
-          <h6>Balance checker widget</h6>
-          <div className="card-content">
-            {this.state.lastAddress ? (
-              <a className="red-text reset-all btn-floating btn-tiny red"<i
-                    className="material-icons tiny right">delete</i></a>
-                ) : ''}
-            <div className="row">
-              <div className="input-field col s12">
-                <input name="address" type="text" />
-                <label>input address</label>
-              </div>
-            </div>
-              <a href="#" className="btn query-balance">Query Balance</a>
-              {/* unfinished goes here */}
-          </div>
-
-      </div>
-  )}
+        return (
+            <If condition={Boolean(this.state.btcPrice)}>
+                <Then><span>${this.state.btcPrice} per&nbsp;Ƀ</span></Then>
+                <Else><Loading /></Else>
+            </If>
+        )
+    }
 }
-/*{{#if lastAddress}}
-  <h6>{{lastAddress}} </h6>
 
-    {{#if lastStatus}}
-    <div className="row">
-      {{#if eq lastStatus 400}} wrong address {{else}} unknown error {{/if}}
-    </div>
-    {{else}}
-
-      {{#each lastData}}
-        <div className="row no-bottom-margin">{{> cgSystemLogo className="logo-portfolio" system=asset}}{{displaySystemName (systemFromId asset)}}:{{quantity}}
-          {{#if or vUsd vBtc}}(
-            {{#if vBtc}}Ƀ{{readableN4 vBtc}}{{/if}}
-            {{#if and vUsd vBtc}}/{{/if}}
-            {{#if vUsd}}${{readableN2 vUsd}}{{/if}}
-          ){{/if}}</div>
-      {{/each}}
-    {{/if}}
-
-{{/if}}
-{{#if queryingAddress}}
-  <p>Querying address: {{queryingAddress}}</p>
-{{/if }}*/
-export default Bitcoin
+export default BitcoinPrice
