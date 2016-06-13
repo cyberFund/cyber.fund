747b77ba13ebd5b2039945da81360da3ac995df1
Mihail Paley <undeadlol1@mail.ru>
2016-06-13 07:36:00 +0300

    add titleComponent property to CrowdSaleCard

--- imports/ui/components/CrowdsaleCardList.js
+++ imports/ui/components/CrowdsaleCardList.js
@@ -2,40 +2,38 @@ import React, { PropTypes } from 'react'
 import CrowdsaleCard from './CrowdsaleCard'
 import { Grid, Cell } from 'react-mdl'
 
-class CrowdsaleCardList extends React.Component {
-  render () {
-    return (
-      <div>
-        <Grid>
-          <Cell col={12} className="text-center">
-            <h5>{this.props.title}</h5>
-          </Cell>
-        </Grid>
-        <Grid>
-        {// if there are any items render them
-          this.props.items.length ?
-          this.props.items.map(item => {
-          return <CrowdsaleCard
-                    key={item._id}
-                    item={item}
-                    {...this.props} />
-                })
-          : null
-        }
-        </Grid>
-      </div>
-  )}
+const CrowdsaleCardList = props => {
+    // title tag is h5 or titleComponent
+    const renderTitle = React.createElement(
+                  props.titleComponent,
+                  {className: "text-center"},
+                  props.title
+                )
+    const renderCards = props.items.map(item => {
+                    return <CrowdsaleCard
+                              key={item._id}
+                              item={item}
+                              {...props} />
+    })
+    return  <div>
+                <Grid>
+                  <Cell col={12}>{renderTitle}</Cell>
+                </Grid>
+                <Grid>
+                    { props.items.length ? renderCards : null }
+                </Grid>
+            </div>
 }
 CrowdsaleCardList.defaultProps = {
-    itemComponent: 'h4'
+    titleComponent: 'h5'
 }
 CrowdsaleCardList.propTypes = {
  title: PropTypes.string.isRequired,
- //itemComponent: PropTypes.oneOf(['active', 'upcoming', 'past', 'projects']),
+ titleComponent: PropTypes.string,
  items: PropTypes.array.isRequired,
  size: PropTypes.oneOf(['big', 'small', '']),
  //type is optional because there is default rule
- type: PropTypes.string
+ type: PropTypes.oneOf(['active', 'upcoming', 'past', 'projects'])
 }
 
 export default CrowdsaleCardList
