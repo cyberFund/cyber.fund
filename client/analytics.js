e2f1aea87fc04d32d201f9a15ad7ee314ed69a12
Mihail Paley <undeadlol1@mail.ru>
2016-06-13 18:03:58 +0300

    add analytics and styles to WelcomePage

---
+++ client/analytics.js
@@ -0,0 +1,83 @@
+var ns = CF.Utils
+
+ns.segmentIoPublicKey = function() {
+  try {
+    return Meteor.settings.public.analyticsSettings["Segment.io"].apiKey;
+  } catch (e) {
+    return "not found";
+  }
+}
+
+! function() {
+  var analytics = window.analytics = window.analytics || [];
+  if (!analytics.initialize)
+    if (analytics.invoked) window.console && console.error && console.error("Segment snippet included twice.");
+    else {
+      analytics.invoked = !0;
+      analytics.methods = ["trackSubmit", "trackClick", "trackLink", "trackForm", "pageview", "identify", "group", "track", "ready", "alias", "page", "once", "off", "on"];
+      analytics.factory = function(t) {
+        return function() {
+          var e = Array.prototype.slice.call(arguments);
+          e.unshift(t);
+          analytics.push(e);
+          return analytics
+        }
+      };
+      for (var t = 0; t < analytics.methods.length; t++) {
+        var e = analytics.methods[t];
+        analytics[e] = analytics.factory(e)
+      }
+      analytics.load = function(t) {
+        var e = document.createElement("script");
+        e.type = "text/javascript";
+        e.async = !0;
+        e.src = ("https:" === document.location.protocol ? "https://" : "http://") + "cdn.segment.com/analytics.js/v1/" + t + "/analytics.min.js";
+        var n = document.getElementsByTagName("script")[0];
+        n.parentNode.insertBefore(e, n)
+      };
+      analytics.SNIPPET_VERSION = "3.0.1";
+      analytics.load(CF.Utils.segmentIoPublicKey());
+
+      // # debug clause.
+      // console.log(CF.Utils.segmentIoPublicKey());
+    }
+}();
+
+
+Template.body.events({
+  'click a[target="_blank"]': function(e, t) {
+    if (analytics) {
+      analytics.track("Clicked External Link", {
+        link: $(e.currentTarget).attr('href')
+      })
+    }
+  }
+});
+
+Meteor.startup(function() {
+  Tracker.autorun(function(c) {
+    var user = Meteor.user();
+    if (!user) {
+      return;
+    }
+
+    var identity = {};
+    if (user.username) {
+      identity.username = user.username;
+    }
+    if (user.profile) {
+      identity.name = user.profile.name;
+    }
+    if (user.emails) {
+      identity.email = user.emails[0].address
+    }
+    if (user.services) {
+      if (user.services.twitter) {
+        identity.twitter = user.services.twitter.screenName;
+      }
+    }
+    analytics.identify(user._id, identity);
+
+    c.stop();
+  });
+});
--- imports/ui/pages/WelcomePage.js
+++ imports/ui/pages/WelcomePage.js
@@ -1,135 +1,149 @@
 import React from 'react'
 import { Meteor } from 'meteor/meteor'
+import { $ } from "meteor/jquery"
 import { Button, Grid, Cell } from 'react-mdl'
 import Brand from '../components/Brand'
 import helpers from '../helpers'
 
-/*Template['welcome'].events({
-  'click .login-button': function (e, t) {
-      analytics.track('redirect to Sign In', {
-        from: 'welcome'
-      });
-      FlowRouter.go('/sign-in')
-  }
-});
-
-componentWillMount: function() {
-    if (Meteor.userId()) FlowRouter.go('profile')
-},*/
 // TODO: test .mdl-cell--stretch on wide element. Dunno if this class works properly
 
-const WelcomePage = props =>{
-    const wideSectionStyle = {color: 'white', width: '100%'}
-    return (
-            <div id="WelcomePage" className="text-center">
-                <Grid style={wideSectionStyle}>
-                    <Cell col={8} tablet={4} phone={12}>
-                        <h1><Brand /></h1>
-                        <h3>Where the World Discovers Blockchain Systems</h3>
-                    </Cell>
-                    <Cell col={4} tablet={4} phone={12}>
-                        <span>
-                            <h5>Control Your Portfolio
-                                <p>Follow Your Favourite Cryptocurrencies</p>
-                                <p>Keep Records of Your Public Keys</p>
-                                <p>Follow the Best Portfolios</p>
-                            </h5>
-                        </span>
-                        <Button href="/sign-in" raised colored ripple>Sign Up with Twitter</Button>
-                    </Cell>
-                </Grid>
-                <Grid>
-                    <Cell col={12}>
-                        <h3>How cyber <span className="red-text">•</span> Fund helps you manage your blockchain portfolio</h3>
-                        <div className="video-container">
-                              <iframe width="853" height="480" src="//www.youtube.com/embed/gGbbawSJ6P0?rel=0" frameborder="0" allowfullscreen>
-                              </iframe>
-                            </div>
-                    </Cell>
-                </Grid>
-                <Grid>
-                    <Cell col={12}>
-                        <h4>Sign up now and your public account will be free forever!</h4>
-                        <Button href="/sign-in" raised colored ripple>Sign Up</Button>
-                    </Cell>
-                </Grid>
-                <Grid>
-                    <Cell col={12}><h2>Why you’ll love <Brand />.</h2></Cell>
-                    <Cell col={4} tablet={4} phone={12}>
-                        <i style={{color: '#ff5722', fontSize: '6em'}} className="material-icons">directions_walk</i>
-                        <h4>Realtime Valuation</h4>
-                        <p>We crunch market data on all blockchains and even for some notable illiquid blockchain assets.</p>
-                    </Cell>
-                    <Cell col={4} tablet={4} phone={12}>
-                        <i style={{color: '#ffeb3b', fontSize: '6em'}} className="material-icons">directions_run</i>
-                        <h4>Balance Autoupdate</h4>
-                        <p>A majority of the blockchains' balances are updated automatically. Insert your public address and it will be tracked.</p>
-                    </Cell>
-                    <Cell col={4} tablet={12} phone={12}>
-                        <i style={{color: '#4CAF50', fontSize: '6em'}} className="material-icons">directions_bike</i>
-                        <h4>Trustless</h4>
-                        <p>We invest a lot in blockains too, so we care about your security. We never touch your private keys.</p>
-                    </Cell>
+class WelcomePage extends React.Component {
+    // redirect user if he is logged in
+    componentWillMount() {
+        if(Meteor.userId()) FlowRouter.go('/profile')
+    }
+    componentDidMount() {
+        // self explanatory
+        $('[href="/sign-in"]').on('click', () => {
+            analytics.track('redirect to Sign In', {
+              from: 'welcome'
+            })
+            FlowRouter.go('/sign-in')
+        })
+    }
+    componentWillUnmount() { // clean up
+        $('[href="/sign-in"]').remove()
+    }
+    render() {
+        const wideSectionStyle = {
+            color: 'white',
+            width: '100%',
+            background: 'url("/images/blockchain.jpg") no-repeat 0 0',
+            backgroundSize: 'cover',
+            minHeight: '300px',
+            padding: '60px 0'
+        }
+        return (
+                <div id="WelcomePage" className="text-center">
+                    <Grid style={wideSectionStyle}>
+                        <Cell col={8} tablet={5} phone={12}>
+                            <h1><Brand /></h1>
+                            <h3>Where the World Discovers Blockchain Systems</h3>
+                        </Cell>
+                        <Cell col={4} tablet={3} phone={12}>
+                            <span>
+                                <h5>Control Your Portfolio
+                                    <p>Follow Your Favourite Cryptocurrencies</p>
+                                    <p>Keep Records of Your Public Keys</p>
+                                    <p>Follow the Best Portfolios</p>
+                                </h5>
+                            </span>
+                            <Button href="/sign-in" raised colored ripple>Sign Up with Twitter</Button>
+                        </Cell>
+                    </Grid>
+                    <Grid>
+                        <Cell col={12}>
+                             <h3>How cyber <span className="red-text">•</span> Fund helps you manage your blockchain portfolio</h3>
+                             <div className="video-container">
+                                 <iframe width="853" height="480" src="//www.youtube.com/embed/gGbbawSJ6P0?rel=0" frameborder="0" allowfullscreen>
+                                 </iframe>
+                             </div>
+                        </Cell>
+                    </Grid>
+                    <Grid>
+                        <Cell col={12}>
+                            <h4>Sign up now and your public account will be free forever!</h4>
+                            <Button href="/sign-in" raised colored ripple>Sign Up</Button>
+                        </Cell>
+                    </Grid>
+                    <Grid>
+                        <Cell col={12}><h2>Why you’ll love <Brand />.</h2></Cell>
+                        <Cell col={4} tablet={4} phone={12}>
+                            <i style={{color: '#ff5722', fontSize: '6em'}} className="material-icons">directions_walk</i>
+                            <h4>Realtime Valuation</h4>
+                            <p>We crunch market data on all blockchains and even for some notable illiquid blockchain assets.</p>
+                        </Cell>
+                        <Cell col={4} tablet={4} phone={12}>
+                            <i style={{color: '#ffeb3b', fontSize: '6em'}} className="material-icons">directions_run</i>
+                            <h4>Balance Autoupdate</h4>
+                            <p>A majority of the blockchains' balances are updated automatically. Insert your public address and it will be tracked.</p>
+                        </Cell>
+                        <Cell col={4} tablet={12} phone={12}>
+                            <i style={{color: '#4CAF50', fontSize: '6em'}} className="material-icons">directions_bike</i>
+                            <h4>Trustless</h4>
+                            <p>We invest a lot in blockains too, so we care about your security. We never touch your private keys.</p>
+                        </Cell>
 
-                    <br className="clearfix mdl-cell--hide-tablet mdl-cell--hide-phone" />
+                        <br className="clearfix mdl-cell--hide-tablet mdl-cell--hide-phone" />
 
-                    <Cell col={4} tablet={12} phone={12}>
-                        <i style={{color: '#2196F3', fontSize: '6em'}} className="material-icons">router</i>
-                        <h4>Scanner</h4>
-                        <p>We scan all(!) blockchain space for prominent crowdfunding and huge deals.</p>
-                    </Cell>
-                    <Cell col={4} tablet={12} phone={12}>
-                        <i style={{color: '#cddc39', fontSize: '6em'}} className="material-icons">toys</i>
-                        <h4>Dispatcher</h4>
-                        <p>We filter the noise so only awesome digital
-                        assets reach your attention. No scum. No kitchen forks. No middleman.</p>
-                    </Cell>
-                    <Cell col={4} tablet={12} phone={12}>
-                        <i style={{color: '#cddc39', fontSize: '6em'}} className="material-icons">grade</i>
-                        <h4>Verifier</h4>
-                        <p>We make due diligence free. Really. View all of our
-                        unique 4 years investment experiences.
-                        Take a look.</p>
-                    </Cell>
-                </Grid>
-                <Grid>
-                    <Cell col={12}>
-                        <h4>Welcome to <Brand />!</h4>
-                        <Button href="/sign-in" raised colored ripple>Dive In</Button>
-                    </Cell>
-                    <Cell col={12}>
-                        <p>{helpers.usersCount()} people already joined</p>
-                    </Cell>
-                </Grid>
-                <Grid>
-                    <Cell col={12}>
-                        <h4>Sign up now and your public account will be free forever!</h4>
-                        <Button href="/sign-in" raised colored ripple>Sign Up</Button>
-                    </Cell>
-                </Grid>
-                <Grid>
-                    <Cell col={12}><h2>For Developers</h2></Cell>
-                </Grid>
-                <Grid>
-                    <Cell col={6} tablet={4} phone={12}>
-                        <i style={{color: '#ffeb3b', fontSize: '6em'}} className="material-icons">stars</i>
-                        <h4>Rating</h4>
-                        If you are a developer you can add cryptocurrency or cryptoasset yourself using our opensource Chaingear library.
-                        <p>
-                            <Button raised colored ripple href="http://cybertalks.org/t/rating-listing-of-cryptocurrencies-and-cryptoassets/353" target="blank">Rating listing</Button>
-                        </p>
-                    </Cell>
-                    <Cell col={6} tablet={4} phone={12}>
-                        <i style={{color: '#F44336', fontSize: '6em'}} className="material-icons">directions_run</i>
-                        <h4>Radar</h4>
-                        You excited about creating or already working on blockchain project you have awesome option to list your project in Radar section of cyber•Fund.
-                        <p>
-                        <Button raised colored ripple href="http://cybertalks.org/t/radar-listing-of-cryptocurrencies-and-cryptoassets/354" target="blank">Radar listing</Button>
-                        </p>
-                    </Cell>
-                </Grid>
-            </div>
-    )
+                        <Cell col={4} tablet={12} phone={12}>
+                            <i style={{color: '#2196F3', fontSize: '6em'}} className="material-icons">router</i>
+                            <h4>Scanner</h4>
+                            <p>We scan all(!) blockchain space for prominent crowdfunding and huge deals.</p>
+                        </Cell>
+                        <Cell col={4} tablet={12} phone={12}>
+                            <i style={{color: '#cddc39', fontSize: '6em'}} className="material-icons">toys</i>
+                            <h4>Dispatcher</h4>
+                            <p>We filter the noise so only awesome digital
+                            assets reach your attention. No scum. No kitchen forks. No middleman.</p>
+                        </Cell>
+                        <Cell col={4} tablet={12} phone={12}>
+                            <i style={{color: '#cddc39', fontSize: '6em'}} className="material-icons">grade</i>
+                            <h4>Verifier</h4>
+                            <p>We make due diligence free. Really. View all of our
+                            unique 4 years investment experiences.
+                            Take a look.</p>
+                        </Cell>
+                    </Grid>
+                    <Grid>
+                        <Cell col={12}>
+                            <h4>Welcome to <Brand />!</h4>
+                            <Button href="/sign-in" raised colored ripple>Dive In</Button>
+                        </Cell>
+                        <Cell col={12}>
+                            <p>{helpers.usersCount()} people already joined</p>
+                        </Cell>
+                    </Grid>
+                    <Grid>
+                        <Cell col={12}>
+                            <h4>Sign up now and your public account will be free forever!</h4>
+                            <Button href="/sign-in" raised colored ripple>Sign Up</Button>
+                        </Cell>
+                    </Grid>
+                    <Grid>
+                        <Cell col={12}><h2>For Developers</h2></Cell>
+                    </Grid>
+                    <Grid>
+                        <Cell col={6} tablet={4} phone={12}>
+                            <i style={{color: '#ffeb3b', fontSize: '6em'}} className="material-icons">stars</i>
+                            <h4>Rating</h4>
+                            If you are a developer you can add cryptocurrency or cryptoasset yourself using our opensource Chaingear library.
+                            <p>
+                                <Button raised colored ripple href="http://cybertalks.org/t/rating-listing-of-cryptocurrencies-and-cryptoassets/353" target="blank">Rating listing</Button>
+                            </p>
+                        </Cell>
+                        <Cell col={6} tablet={4} phone={12}>
+                            <i style={{color: '#F44336', fontSize: '6em'}} className="material-icons">directions_run</i>
+                            <h4>Radar</h4>
+                            You excited about creating or already working on blockchain project you have awesome option to list your project in Radar section of cyber•Fund.
+                            <p>
+                            <Button raised colored ripple href="http://cybertalks.org/t/radar-listing-of-cryptocurrencies-and-cryptoassets/354" target="blank">Radar listing</Button>
+                            </p>
+                        </Cell>
+                    </Grid>
+                </div>
+        )
+    }
 }
 
 
