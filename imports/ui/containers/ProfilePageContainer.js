37c4a566ac66140ab631a5898389bda46a34ffb0
Mihail Paley <undeadlol1@mail.ru>
2016-06-20 22:48:05 +0300

    add functions to ProfilePageContainer

--- imports/ui/containers/ProfilePageContainer.js
+++ imports/ui/containers/ProfilePageContainer.js
@@ -1,61 +1,76 @@
 import React from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
 import { Meteor } from 'meteor/meteor'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import get from 'oget'
 import ProfilePage from '../pages/ProfilePage'
 
 export default ProfilePageContainer = createContainer(() => {
-  const loaded = Meteor.subscribe('crowdsalesAndProjectsList').ready()
-  const {selectors} = CF.CurrentData,
-          crowdsales = CurrentData.find(selectors.crowdsales()).fetch(),
-          projects =  CurrentData.find({
-            $or: [{
-              'crowdsales.start_date': {
-                $gt: new Date()
-              }
-            }, {
-              'crowdsales.start_date': {
-                $exists: false
-              }
-            }],
-            $and: [
-            selectors.projects()]
-          }, {
-            sort: {
-              "calculatable.RATING.vector.LV.sum": -1
-            }
-          }).fetch(),
-          past = CurrentData.find({
-            $and: [selectors.crowdsales(), {
-              'crowdsales.end_date': {
-                $lt: new Date()
-              }
-            }]
-          }, {
-            sort: {
-              'crowdsales.end_date': -1
-            }
-          }).fetch(),
-          upcoming = CurrentData.find({
-            $and: [selectors.crowdsales(), {
-              'crowdsales.start_date': {
-                $gt: new Date()
-              }
-            }]
-          }, {
-            sort: {
-              'crowdsales.start_date': 1
-            }
-          }).fetch(),
-          active = CurrentData.find({
-            $and: [selectors.crowdsales(), {
-              'crowdsales.end_date': {
-                $gt: new Date()
-              }
-            }, {
-              'crowdsales.start_date': {
-                $lt: new Date()
-              }
-            }]
-          }, {sort: {"metrics.currently_raised": -1}}).fetch()
-  return {crowdsales, projects, past, upcoming, active, loaded}
+    /*const username = FlowRouter.getParam('username'),
+            loaded = Meteor.subscribe('friendlyUsers', {username}).ready() &&
+                     Meteor.subscribe('portfolioSystems', {username}).ready() &&
+                     Meteor.subscribe('userProfile', {username}).ready() //CF.subs.Assets =
+
+  // TODO get rid of Tracker.autorun
+  Tracker.autorun(function() {
+        if (CF.UserAssets.graph && CF.UserAssets.graph.folioPie){ //crutch
+          CF.UserAssets.graph.folioPie.update({
+            labels: [],
+            series: []
+          })
+        }
+        if (username) Meteor.call("cfAssetsUpdateBalances", {username})
+  })
+
+  Tracker.autorun(function() {
+        const user = CF.User.findOneByUsername(username)
+        const name = get(user, 'profile.name', username)
+        document.title = name + ' - ' + 'cyber•Fund';
+  })
+
+    const userAccounts = CF.Accounts.findByRefId(CF.Profile.currentUid()).fetch(),
+        profileName = this.profile && this.profile.name, // what is 'this.profile'?
+        userRegistracionCount = Session.get("userRegistracionCount"),
+        isOwnProfile = !Meteor.userId() ? false :(CF.Profile.currentUsername() == CF.User.username()),
+        user = _user()
+        /*following  = //whether current user is followed by client user
+      var user = Meteor.user();
+      if (!Meteor.user()) return false;
+      return user.profile && user.profile.followingUsers &&
+        _.contains(user.profile.followingUsers, CF.Profile.currentUid())
+    ,
+    followingCount: function() { //
+      var user = _user();
+      return user.profile && user.profile.followingUsers && user.profile.followingUsers.length || 0
+    },
+    starred: function() {
+      var user = _user();
+      return user.profile && user.profile.starredSystems &&
+        CurrentData.find(cfCDs.system(user.profile.starredSystems)) || []
+    },
+    followingUsers: function() {
+      var user = _user();
+      return user.profile && user.profile.followingUsers &&
+        Meteor.users.find({
+          _id: {
+            $in: user.profile.followingUsers
+          }
+        }) || []
+    },
+    followedByUsers: function() {
+      var user = _user();
+      return user.profile && user.profile.followedBy &&
+        Meteor.users.find({
+          _id: {
+            $in: user.profile.followedBy
+          }
+        }) || []
+    },
+    followedByCount: function() {
+      var user = _user();
+      return user.profile && user.profile.followedBy && user.profile.followedBy.length || 0
+    }
+  });*/
+
+  return {loaded: true}
 }, ProfilePage)
