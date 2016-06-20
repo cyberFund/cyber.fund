b0ed7c3c098e519e473568c084b038ecc03f0fc5
Mihail Paley <undeadlol1@mail.ru>
2016-06-20 22:00:22 +0300

    add tabs to ProfilePage

--- imports/ui/pages/ProfilePage.js
+++ imports/ui/pages/ProfilePage.js
@@ -1,34 +1,90 @@
-import React from 'react'
-import { Grid, Cell } from 'react-mdl'
+import React, { Component, PropTypes }  from 'react'
+import { Grid, Cell, FABButton, Icon, Button, Tabs, Tab } from 'react-mdl'
+import {Hide, Show} from '../components/Utils'
+import Brand from '../components/Brand'
 import Loading from '../components/Loading'
+import PortfolioTable from '../components/PortfolioTable'
 
-const ProfilePage = props => {
-    return props.loaded ? (
-          <Grid id="LoginPage">
-              {/* USER INFO */}
-              <Cell col={12}>
-                  avatar
-                  name
-                  info
-                  starred
-                  followers
-                  following
-                  logout button
-              </Cell>
-              {/* PORTFOLIO */}
-              <Cell col={12}>
-                  tab 1:
-                  header
-                  video
-                  graph
-              </Cell>
-              {/* ACCOUNTS */}
-              <Cell col={12}>
-                  tab:
-                  blank
-              </Cell>
-          </Grid>
-    ) : <Loading />
+class ProfilePage extends Component {
+    constructor(props) {
+    super(props)
+    this.state = { activeTab: 0 };
+    }
+    render() {
+        const {props, state} = this
+        const tabContent = ()=> {
+            if (state.activeTab == 0) {
+                return <section>
+                                <h3>Welcome to <Brand />!! Here is short video to help you get started.</h3>
+                                <div className="video-container">
+                                      <iframe width="853" height="480" src="//www.youtube.com/embed/VPQhbLOQIyc?rel=0" frameborder="0" allowfullscreen></iframe>
+                                </div>
+                                <PortfolioTable />
+                        </section>
+            }
+            else return <section>
+                            {/* ACCOUNTS */}
+                            {/* nothing here yet */}
+                            <i>nothing here yet...</i>
+                        </section>
+        }
+        return props.loaded ? (
+              <Grid id="LoginPage">
+                  {/* USER INFO */}
+                  <Cell col={3} tablet={12} className="mdl-cell--order-12-tablet">
+                      <p>avatar</p>
+                      <p>name</p>
+                      <p>info</p>
+                      <p>starred</p>
+                      <p>followers</p>
+                      <p>following</p>
+                      <p>logout button</p>
+                      <Button raised colored ripple>Logout</Button>
+                  </Cell>
+                  <Cell col={9} tablet={12} className="text-center">
+                    <Tabs activeTab={state.activeTab} onChange={tabId => this.setState({ activeTab: tabId })} ripple>
+                        <Tab>Portfolio</Tab>
+                        <Tab>Accounts</Tab>
+                    </Tabs>
+                    {tabContent()}
+                  </Cell>
+                  {/* RED ROUND BUTTON */}
+                    <FABButton colored ripple style={{position: 'fixed', right: 24, bottom: 24}}>
+                        <Icon name="add" />
+                    </FABButton>
+              </Grid>
+        ) : <Loading />
+    }
+    /*Template['profile'].events({
+      'click .btn-follow': function(e, t) {
+        analytics.track('Followed Person', {
+          personName: CF.Profile.currentUsername()
+        });
+        if (!Meteor.user()) FlowRouter.go("/welcome");
+        Meteor.call('followUser', CF.Profile.currentUid())
+      },
+      'click .btn-unfollow': function(e, t) {
+        analytics.track('Unfollowed Person', {
+          personName: CF.Profile.currentUsername()
+        });
+        if (!Meteor.user()) FlowRouter.go("/welcome");
+        Meteor.call('followUser', CF.Profile.currentUid(), {
+          unfollow: true
+        })
+      },
+      'click #at-nav-button': function(e, t) {
+        analytics.track('Sign Out', {
+          from: 'profile'
+        });
+        Meteor.logout()
+      }
+    });*/
+
+}
+
+ProfilePage.propTypes = {
+ //skills: PropTypes.array.isRequired,
+ //numberOfSkills: PropTypes.number.isRequired
 }
 
 export default ProfilePage
