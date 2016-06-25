3e74f423966008caf92526965a2ba80dcd50c9bd
Mihail Paley <undeadlol1@mail.ru>
2016-06-25 04:19:00 +0300

    refactor ProfilePageContainer

--- imports/ui/containers/ProfilePageContainer.js
+++ imports/ui/containers/ProfilePageContainer.js
@@ -15,7 +15,7 @@ export default ProfilePageContainer = createContainer(() => {
                      Meteor.subscribe('portfolioSystems', {username}).ready() &&
                      Meteor.subscribe('userProfile', {username}).ready()
 
-// TODO refactoring, add comments
+    // TODO refactoring, add comments
 
 	if (CF.UserAssets.graph && CF.UserAssets.graph.folioPie){ //crutch
 	  CF.UserAssets.graph.folioPie.update({
@@ -28,38 +28,31 @@ export default ProfilePageContainer = createContainer(() => {
 
 
 	// TODO which constants are actually used and which are not?
-	const userAccounts = CF.Accounts.findByRefId(CF.Profile.currentUid()).fetch(),
-    //profileName = this.profile && this.profile.name
-    userRegistracionCount = Session.get("userRegistracionCount"),
-    isOwnProfile = Meteor.userId() ? (CF.Profile.currentUsername() == CF.User.username()) : false,
-    followingCount = get(user, 'profile.followingUsers.length', 0),
-    starred = CurrentData.find(
-		CF.CurrentData.selectors.system(
-			get(user, 'profile.starredSystems', []))).fetch(),
-    followingUsers = Meteor.users.find({
-          _id: {
-            $in: get(user, 'profile.followingUsers', [])
-          }
-	  }).fetch(),
-    followedByUsers = Meteor.users.find({
-          _id: {
-            $in: get(user, 'profile.followedBy', [])
-          }
-	  }).fetch(),
-    followedByCount = get(user, 'profile.followedBy', 0),
-    //whether current user is followed by client user
-    following = _.contains(get(user, 'profile.followingUsers', []), CF.Profile.currentUid())
+	//profileName = this.profile && this.profile.name
 
   return {
-	  user,
-	  loaded,
-	  userRegistracionCount,
-	  isOwnProfile,
-	  followingCount,
-	  starred,
-	  followingUsers,
-	  followedByUsers,
-	  followedByCount,
-	  following
+		user,
+		loaded,
+		userAccounts: CF.Accounts.findByRefId(CF.Profile.currentUid()).fetch(),
+		userRegistracionCount: Session.get("userRegistracionCount"),
+		isOwnProfile: Meteor.userId() ? (CF.Profile.currentUsername() == CF.User.username()) : false,
+		starred: CurrentData.find(
+			CF.CurrentData.selectors.system(
+				get(user, 'profile.starredSystems', [])
+			)).fetch(),
+		followingUsers: Meteor.users.find({
+			    _id: {
+			      $in: get(user, 'profile.followingUsers', [])
+			    }
+			}).fetch(),
+		followedByUsers: Meteor.users.find({
+			    _id: {
+			      $in: get(user, 'profile.followedBy', [])
+			    }
+			}).fetch(),
+		followingCount: get(user, 'profile.followingUsers.length', 0),
+		followedByCount: get(user, 'profile.followedBy', 0),
+		//whether current user is followed by client user
+		following: _.contains(get(user, 'profile.followingUsers', []), CF.Profile.currentUid())
   }
 }, ProfilePage)
