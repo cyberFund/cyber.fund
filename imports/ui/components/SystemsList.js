16fbc01bd6f7305f32009bac7a0eae9a525bd63a
Mihail Paley <undeadlol1@mail.ru>
2016-06-25 04:18:04 +0300

    add SystemsList

---
+++ imports/ui/components/SystemsList.js
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react'
+import helpers from '../helpers'
+import Image from '../components/Image'
+
+const SystemsList = props => {
+	const systems = props.systems.map( system => {
+		return  <a
+					key={system._id}
+					href={`/system/${helpers._toUnderscores(system._id)}`}
+					title={system._id}>
+					<Image src={CF.Chaingear.helpers.cgSystemLogoUrl(system)} avatar />
+				</a>
+	})
+    return <div {...props}>{systems}</div>
+}
+
+SystemsList.defaultProps = {
+	systems: []
+}
+
+SystemsList.propTypes = {
+ systems: PropTypes.array.isRequired
+}
+
+export default SystemsList
