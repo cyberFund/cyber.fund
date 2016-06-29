153f0666f6f68a1116bc532cb01292410d1a6dd8
Mihail Paley <undeadlol1@mail.ru>
2016-06-29 13:31:53 +0500

    add "component" property to utility components

--- imports/ui/components/Utils.js
+++ imports/ui/components/Utils.js
@@ -3,22 +3,32 @@ import React, { PropTypes } from 'react'
 function checkValue (value) {
     return Boolean(value)
 }
-/* 	checkValue and showOrHide not merged because in future checkValue
+/* 	checkValue() and render() not merged because in future checkValue
 	might have more checks and/or will be used elsewhere */
-function showOrHide(condition, children) {
-	    if (condition) return children
-	    else return null
+function render(condition, props) {
+	// if component specified create custom element
+	if (condition && props.component) return <props.component {...props} />
+	// else return children or nothing
+	else return condition ? props.children : null
 }
-// <Show condition={true}> {props.chilcredn} </Show> == show element
+// <Show condition={true}> {props.children} </Show> == show element
 const Show = props => {
-    return showOrHide(checkValue(props.condition), props.children)
+    return render(checkValue(props.condition), props)
 }
-// <Hide condition={true}> {props.chilcredn} </Hide> == hide element
+// <Hide condition={true}> {props.children} </Hide> == hide element
 const Hide = props => {
-	return showOrHide(!checkValue(props.condition), props.children)
+	return render(!checkValue(props.condition), props)
 }
-// <If condition={true}> {props.chilcredn} </If> == show element
+// <If condition={true}> {props.children} </If> == show element
 const If = props => {
-    return showOrHide(checkValue(props.condition), props.children)
+    return render(checkValue(props.condition), props)
 }
-export { Show, Hide, If }
+// <Unless condition={true}> {props.children} </Unless> == hide element
+const Unless = props => {
+    return render(
+			!checkValue(props.condition),
+			createElement(props.element, props.children)
+		)
+}
+
+export { Show, Hide, If, Unless }
