102666de468be51f399b414d148f4a1f96e8f259
Mihail Paley <undeadlol1@mail.ru>
2016-06-29 13:56:51 +0500

    intermediate fix for PortfolioTable

--- imports/ui/components/PortfolioTable.js
+++ imports/ui/components/PortfolioTable.js
@@ -1,48 +1,86 @@
 import React, { PropTypes } from 'react'
-import {DataTable, TableHeader} from 'react-mdl'
+import {Grid, Cell} from 'react-mdl'
+import helpers from '../helpers'
+import Image from '../components/Image'
+
+/*Template["portfolioWidgetTable"].events({
+  "click th.sorter": function (e, t) {
+    var newSorter = $(e.currentTarget).data("sorter");
+    var sort = CF.Utils._session.get("folioWidgetSort");
+    // same sorting criteria - reverse order
+    if (sort[newSorter]) {
+      sort[newSorter] = -sort[newSorter];
+    } else {
+      sort = {};
+      sort[newSorter] = -1;
+    }
+    analytics.track("Sorted Portfolio", {
+      sort: sort
+    });
+    CF.Utils._session.set("folioWidgetSort", sort);
+  }
+});*/
 
 const PortfolioTable = props => {
-    let data
-    try {
-      data = props.systems.map( i => {
-          return {
-            system: helpers.displaySystemName(i),
-            cmgr: helpers.readableN1(i.calculatable.RATING.vector.GR.monthlyGrowthD),
-            months: helpers.readableN0(i.calculatable.RATING.vector.GR.months),
-            rating: helpers.readableN1(i.calculatable.RATING.sum)
-          }
-      })
-    } catch (e) { data = [] }
-    return (
-        <DataTable rows={data} style={tableStyle} shadow={0}>
-            <TableHeader name="system">
-              System
-            </TableHeader>
-            <TableHeader numeric name="amount">
-              Amount
-            </TableHeader>
-            <TableHeader numeric name="equity">
-              Equity
-            </TableHeader>
-            <TableHeader numeric name="share">
-              Portfolio Share
-            </TableHeader>
-            <TableHeader numeric name="btc">
-              Value in BTC
-            </TableHeader>
-            <TableHeader numeric name="usd">
-              Value in USD
-            </TableHeader>
-            <TableHeader numeric name="usdPrice">
-              USD Price
-            </TableHeader>
-            <TableHeader numeric name="usdCap">
-              USD Cap
-            </TableHeader>
-        </DataTable>
-    )
+	console.log(props.systems())
+	// render rows with data
+  	function renderRows() {
+		return props.systems.map( system => {
+			return  <tr key={system._id}>
+						<td className="mdl-data-table__cell--non-numeric">
+							<a href={`/system/${helpers._toUnderscores(system._id)}`}>
+								<Image
+									avatar
+									src={CF.Chaingear.helpers.cgSystemLogoUrl(system)}
+									style={{marginRight: 24}}
+									/>
+								<span>{helpers.displaySystemName(system)}</span>
+							</a>
+						</td>
+						<td>
+							{helpers.readableN2(system.quantity)}
+						</td>
+						<td>
+							{helpers.readableN3(system.equity)}
+						</td>
+						<td>
+							{helpers.percents1(system.share)}
+						</td>
+						<td>
+							{helpers.readableN2(system.btcCost)}
+						</td>
+						<td>
+							{helpers.readableN0(system.usdCost)}
+						</td>
+					</tr>
+		})
+	}
+    return  <Grid>
+				<Cell col={12}>
+					<table className="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp center" {...props}>
+						<thead>
+							<tr>
+								<th style={{textAlign: 'center'}} className="mdl-data-table__cell--non-numeric">
+								    System
+								</th>
+								<th>Amount</th>
+								<th>Equity</th>
+								<th>Portfolio Share</th>
+								<th>Value in BTC</th>
+								<th>USD Price<sup>1d Change</sup></th>
+							</tr>
+						</thead>
+						<tbody>
+							{renderRows()}
+						</tbody>
+					</table>
+				</Cell>
+	          {/* you can add components after table */}
+	          {props.children}
+	        </Grid>
 }
-/*PortfolioTable.defaultProps = {
-    component: 'span'
-}*/
+// TODO do not forget to add props
+PortfolioTable.propTypes = {
+    //component: 'span'
+}
 export default PortfolioTable
