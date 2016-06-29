fa4e2c91f80f9fb1da4366cf37b1420b4d1a8ace
Mihail Paley <undeadlol1@mail.ru>
2016-06-29 13:49:04 +0500

    intermediate fix of data fetching in SystemPageContainer

--- imports/ui/containers/SystemPageContainer.js
+++ imports/ui/containers/SystemPageContainer.js
@@ -1,61 +1,195 @@
 import React from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
 import { Meteor } from 'meteor/meteor'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import get from 'oget'
+import helpers from '../helpers'
 import SystemPage from '../pages/SystemPage'
 
 export default SystemPageContainer = createContainer(() => {
-  const loaded = Meteor.subscribe('crowdsalesAndProjectsList').ready()
-  const {selectors} = CF.CurrentData,
-          crowdsales = CurrentData.find(selectors.crowdsales()).fetch(),
-          projects =  CurrentData.find({
-            $or: [{
-              'crowdsales.start_date': {
-                $gt: new Date()
-              }
-            }, {
-              'crowdsales.start_date': {
-                $exists: false
-              }
-            }],
-            $and: [
-            selectors.projects()]
-          }, {
-            sort: {
-              "calculatable.RATING.vector.LV.sum": -1
-            }
-          }).fetch(),
-          past = CurrentData.find({
-            $and: [selectors.crowdsales(), {
-              'crowdsales.end_date': {
-                $lt: new Date()
-              }
-            }]
-          }, {
-            sort: {
-              'crowdsales.end_date': -1
-            }
-          }).fetch(),
-          upcoming = CurrentData.find({
-            $and: [selectors.crowdsales(), {
-              'crowdsales.start_date': {
-                $gt: new Date()
-              }
-            }]
-          }, {
-            sort: {
-              'crowdsales.start_date': 1
-            }
-          }).fetch(),
-          active = CurrentData.find({
-            $and: [selectors.crowdsales(), {
-              'crowdsales.end_date': {
-                $gt: new Date()
-              }
-            }, {
-              'crowdsales.start_date': {
-                $lt: new Date()
-              }
-            }]
-          }, {sort: {"metrics.currently_raised": -1}}).fetch()
-  return {crowdsales, projects, past, upcoming, active, loaded}
+	// functions
+	function systemName() {
+	  return helpers._toSpaces(FlowRouter.getParam("name_"))
+	}
+	function curData() {
+	  return CurrentData.findOne({
+	    _id: systemName()
+		})
+	}
+	// variables
+	const cfCDs = CF.CurrentData.selectors
+	const loaded = Meteor.subscribe("systemData", {
+						name: systemName()
+					}).ready() &&
+					Meteor.subscribe("dependentCoins", systemName()).ready()
+	const data = curData()
+
+	if (data && data.dependencies) {
+		var d = data.dependencies;
+		if (!_.isArray(d)) d = [d];
+		if (d.indexOf("independent") == -1) {
+		  Meteor.subscribe("dependencies", d);
+		}
+	}
+
+/* what does this code even do???
+	Meteor.startup(function() {
+	  CF.keenflag = new ReactiveVar();
+	  Keen.ready(function() {
+	    CF.keenflag.set(true);
+	  });
+	});
+*/
+
+	// rework needed
+
+  return {
+	  	loaded,
+		system: curData() || {},
+
+		/* THIS IS OLD SHIT */
+	  yesterdaySupplyMetric: function(){
+		var m = this.metrics;
+		return (m.supply - m.supplyChange.day) || 0;
+	  },
+	  anyCards: function(){
+		if (!this.metrics) return false;
+		var m = this.metrics;
+		return m.tradeVolume || m.supply ||
+		(m.price && (m.price.usd || m.price.btc || m.price.eth)) ||
+		(m.cap && (m.cap.usd || m.cap.btc || m.cap.eth ));
+	  },
+	  systemName: systemName(),
+	  dependents: function() {
+		return CurrentData.find(cfCDs.dependents(systemName()), {
+		  sort: {
+			_id: 1
+		  }
+		});
+	  },
+
+	  depends_on: function() {
+		var self = curData();
+		if (!self.dependencies) return [];
+		var deps = self.dependencies;
+		if (!_.isArray(deps)) deps = [deps];
+		return CurrentData.find(cfCDs.dependencies(deps));
+	  },
+
+	  dependentsExist: function() {
+		return CurrentData.find(cfCDs.dependents(systemName())).count();
+	  },
+
+	  symbol: function() {
+		return this.token ? this.token.symbol : "";
+	  },
+
+	  hashtag: function() {
+		return (this.descriptions && this.descriptions.hashtag) ? this.descriptions.hashtag.slice(1) : "";
+	  },
+	  existLinksWith: function(links, tag) {
+		if (!_.isArray(links)) return false;
+		return !!_.find(links, function(link) {
+		  return (_.isArray(link.tags) && link.tags.indexOf(tag) > -1);
+		});
+	  },
+
+	  mainTags: function() {
+		return ["Wallet", "Exchange", "Analytics", "Magic"];
+	  },
+
+	  linksWithoutTags: function(links, tags) {
+		if (!_.isArray(links)) return [];
+
+		return _.filter(links, function(link) {
+		  var ret = _.isArray(link.tags);
+		  _.each(tags, function(tag) {
+			if (link.tags.indexOf(tag) > -1) ret = false;
+		  });
+
+		  return ret;
+		});
+	  },
+
+	  isProject: function() {
+		return this.descriptions && this.descriptions.state == "Project";
+	  },
+
+	  // todo: currently, those are using current price to estimate yesterday' trade volume.
+	  // not good. must be fixed.
+	  todayVolumeUsd: function() {
+		if (this.metrics && this.metrics.tradeVolume && this.metrics.price) {
+		  return this.metrics.tradeVolume * this.metrics.price.usd / this.metrics.price.btc;
+		}
+		return 0;
+	  },
+	  yesterdayVolumeUsd: function() {
+		var metrics = this.metrics;
+		if (metrics && metrics.tradeVolumePrevious &&
+		  metrics.tradeVolumePrevious.day && metrics.price) {
+		  return metrics.price.usd / metrics.price.btc * metrics.tradeVolumePrevious.day;
+		}
+	  },
+	  todayVolumeBtc: function() {
+		if (this.metrics && this.metrics.tradeVolume && this.metrics.price) {
+		  return this.metrics.tradeVolume;
+		}
+		return 0;
+	  },
+	  yesterdayVolumeBtc: function() {
+		var metrics = this.metrics;
+		if (metrics && metrics.tradeVolumePrevious &&
+		  metrics.tradeVolumePrevious.day && metrics.price) {
+		  return metrics.tradeVolumePrevious.day;
+		}
+	  },
+	  usdVolumeChange: function() {
+		var metrics = this.metrics;
+		if (metrics && metrics.tradeVolumePrevious &&
+		  metrics.tradeVolumePrevious.day && metrics.price && metrics.tradeVolume && metrics.price) {
+
+		  return CF.Utils.deltaPercents(metrics.price.usd / metrics.price.btc * metrics.tradeVolume,
+			metrics.price.usd / metrics.price.btc * metrics.tradeVolumePrevious.day);
+		}
+	  },
+	  btcVolumeChange: function() {
+		var metrics = this.metrics;
+		if (metrics && metrics.tradeVolumePrevious &&
+		  metrics.tradeVolumePrevious.day && metrics.tradeVolume) {
+
+		  return CF.Utils.deltaPercents(metrics.tradeVolume,
+			metrics.tradeVolumePrevious.day);
+		}
+	  },
+	  main_links: function() {
+		if (!this.links || !_.isArray(this.links)) {
+		  return [];
+		}
+
+		return _.first(_.filter(this.links, function(link) {
+		  return (link.tags && _.isArray(link.tags) && link.tags.indexOf("Main") > -1);
+		}), 4);
+	  },
+	  ___join: function(k1, k2) {
+		return k1 + "_" + k2;
+	  },
+	  systemIsStarredColor: function() {
+		var ret = false;
+		var user = Meteor.user();
+		if (user && user.profile && user.profile.starredSystems) {
+		  ret = user.profile.starredSystems.indexOf(this._id) > -1;
+		}
+		return ret ? "yellow" : "grey";
+	  },
+	  dailyData: function() {
+		var _id = this._id;
+		return FastData.find({
+		  systemId: _id
+		}, {
+		  sort: {
+			timestamp: 1
+		  }
+		});
+	  }
+  }
 }, SystemPage)
