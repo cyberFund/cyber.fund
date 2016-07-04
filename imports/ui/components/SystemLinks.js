ee428b3c4ff400247cdebc8bc12e0bcc5c4dc232
Mihail Paley <undeadlol1@mail.ru>
2016-07-04 21:04:32 +0500

    intermediate SystemLinks fix

--- imports/ui/components/SystemLinks.js
+++ imports/ui/components/SystemLinks.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 import { Grid, Cell, Icon, Button, Tabs, Tab } from 'react-mdl'
 import { If } from '../components/Utils'
+import helpers from '../helpers'
 
 class SystemLinks extends React.Component {
 	constructor(params) {
@@ -14,22 +15,59 @@ class SystemLinks extends React.Component {
 	changeTab(activeTab) {
 		this.setState({activeTab})
 	}
+	renderLinksArea(tag) {
+		function renderLinks(tag) {
+			//helpers.existLinksWith
+
+			return this.props.links.map( (link, i) => <p key={i}>{link.url}</p>)
+		}
+		switch (tag) {
+			case 0:
+				return renderLinks("Exchange")
+			case 1:
+				return renderLinks("Wallet")
+			case 2:
+				return renderLinks("Analytics")
+			case 3:
+				return renderLinks("Magic")
+			default:
+				return renderLinks()
+		}
+	}
 	render() {
-		const {state, props, setState, changeTab, displayTabs} = this
+		const {state, props: {links}, setState, changeTab } = this
+		const tags = ['Exchange', 'Wallet', 'Analytics', 'Magic']
+		// TODO maybe create tabs with vanilla mdl instead of react-mdl?
 		return  <Grid className="text-center">
-					<Cell col={12} tablet={8} phone={4} >
+					<Cell col={12}>
 						{/* TAB SELECTOR */}
 						<If condition={state.displayTabs}>
 							<Tabs activeTab={state.activeTab} onChange={changeTab.bind(this)} ripple>
-								<Tab>Buy</Tab>
-								<Tab>Hold</Tab>
-								<Tab>Analyze</Tab>
-								<Tab>Earn</Tab>
+								<If
+									condition={helpers.existLinksWith(links, 'Exchange')}
+									component={Tab}>
+									Buy
+								</If>
+								<If
+									condition={helpers.existLinksWith(links, 'Wallet')}
+									component={Tab}>
+									Hold
+								</If>
+								<If
+									condition={helpers.existLinksWith(links, 'Analytics')}
+									component={Tab}>
+									Analyze
+								</If>
+								<If
+									condition={helpers.linksWithoutTags(links, tags)}
+									component={Tab}>
+									Earn
+								</If>
 							</Tabs>
 						</If>
 						{/* CONTENT */}
 						<section>
-							  {props.links.map( (link, index) => <p key={index}>{link.name}</p> )}
+							  {links.map( (link, index) => <p key={index}>{link.name}</p> )}
 						</section>
 					</Cell>
 				</Grid>
