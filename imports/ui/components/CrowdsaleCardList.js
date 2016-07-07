2a39e3fb411091fe4ece6eaf8699aff135aaee29
Mihail Paley <undeadlol1@mail.ru>
2016-07-07 21:59:05 +0500

    (CrowdsaleCardList) fix wrong rendering, refactor

--- imports/ui/components/CrowdsaleCardList.js
+++ imports/ui/components/CrowdsaleCardList.js
@@ -4,23 +4,21 @@ import { Grid, Cell } from 'react-mdl'
 
 const CrowdsaleCardList = props => {
 	// self explanatory
-	if (!props.items) return null
+	if (props.items.length == 0) return null
     // title tag is h5 or titleComponent
-    const renderTitle = React.createElement(
-                  props.titleComponent,
-                  {className: "text-center"},
-                  props.title
-                )
-    const renderCards = props.items.map(item => {
-                    return <CrowdsaleCard
-                              key={item._id}
-                              item={item}
-                              {...props} />
-    })
+    const renderTitle = <props.titleComponent className="text-center">
+							{props.title}
+						</props.titleComponent>
+	function renderCards() {
+		return props.items.map(
+			item => <CrowdsaleCard key={item._id} item={item} {...props} />
+		)
+	}
+
     // if col width specified render component ass Cell instead of Grid
     return  <div className={props.col ? `mdl-cell mdl-cell--${props.col}-col` : 'mdl-grid'}>
                   <Cell col={12}>{renderTitle}</Cell>
-                  {props.items.length ? renderCards : null}
+                  {props.items.length ? renderCards() : null}
             </div>
 }
 CrowdsaleCardList.defaultProps = {
