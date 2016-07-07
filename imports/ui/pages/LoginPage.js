c02ac765c40ae762af308006fdddae2690735513
Mihail Paley <undeadlol1@mail.ru>
2016-07-07 21:54:36 +0500

    (atForm) center component

--- imports/ui/pages/LoginPage.js
+++ imports/ui/pages/LoginPage.js
@@ -3,13 +3,11 @@ import Blaze from 'meteor/gadicc:blaze-react-component'
 import { Grid, Cell } from 'react-mdl'
 
 const LoginPage = props => {
-    return (
-          <Grid id="LoginPage">
-              <Cell col={12}>
-                  <Blaze template="atForm" />
-              </Cell>
-          </Grid>
-    )
+    return 	<Grid id="LoginPage">
+				<section className="mdl-shadow--4dp hover-shadow">
+            		<Blaze template="atForm" />
+				</section>
+			</Grid>
 }
 
 export default LoginPage
