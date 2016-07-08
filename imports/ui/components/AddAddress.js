0c7d8bb0270878480811751a422c9939436d5c2b
Mihail Paley <undeadlol1@mail.ru>
2016-07-08 19:58:43 +0500

    (AddAddress) add inputs to form, add data fetching

--- imports/ui/components/AddAddress.js
+++ imports/ui/components/AddAddress.js
@@ -1,66 +1,114 @@
 import React, { PropTypes } from 'react'
 import { Meteor } from 'meteor/meteor'
-import { FlowRouter } from 'meteor/kadira:flow-router'
-import { _ } from 'meteor/underscore'
-import get from 'oget'
+
+import { If, Hide } from '../components/Utils'
+import { Grid, Cell, Switch, RadioGroup, Radio } from 'react-mdl'
 
 import Dialog from 'material-ui/Dialog'
 import FlatButton from 'material-ui/FlatButton'
 import RaisedButton from 'material-ui/RaisedButton'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import ContentAdd from 'material-ui/svg-icons/content/add'
+import Toggle from 'material-ui/Toggle'
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton'
+import ActionFavorite from 'material-ui/svg-icons/action/favorite'
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border'
+import Subheader from 'material-ui/Subheader'
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
+import TextField from 'material-ui/TextField'
 
-/**
- * Dialog with action buttons. The actions are passed in as an array of React objects,
- * in this example [FlatButtons](/#/components/flat-button).
- *
- * You can also close this dialog by clicking outside the dialog, or with the 'Esc' key.
- */
 class AddAddress extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = { open: false }
-		this.handleClose = this.handleClose.bind(this)
-		this.handleOpen = this.handleOpen.bind(this)
-	}
-	handleOpen() {
-		this.setState({open: true})
+	  // todo: check user has required thing marked.
+	  // not to keep this in profile, as profile intended to be user-modifiable
+		const user = Meteor.user();
+		const hasAccess = CF.UserAssets.isPrivateAccountsEnabled(user) && CF.User.hasPublicAccess(user)
+
+		this.state = {
+			open: false,
+			addressError: '',
+			nameError: '',
+			disabledTogglePrivacy: hasAccess ? '' : 'disabled',
+			privacyState: CF.User.hasPublicAccess(user) ? '' : 'checked',
+			userAccounts: CF.Accounts.findByRefId(Meteor.userId())
+		}
+		this.toggleDialog = this.toggleDialog.bind(this)
+		this.handleSubmit = this.handleSubmit.bind(this)
 	}
 
-	handleClose() {
-		this.setState({open: false})
+	toggleDialog() {
+		this.setState({open: !this.state.open})
+	}
+
+	handleSubmit() {
+		console.log("handleSubmit is fired!")
 	}
 
 	render() {
-		const { handleOpen, handleClose, state } = this
-		const actions = [
-		  <FlatButton
-		    label="Cancel"
-		    primary={true}
-		    onTouchTap={handleClose}
-		  />,
-		  <FlatButton
-		    label="Submit"
-		    primary={true}
-		    keyboardFocused={true}
-		    onTouchTap={handleClose}
-		  />
-		]
+		const { toggleDialog, handleSubmit, state } = this
 
-		return 	<div>
-					<FloatingActionButton secondary={true} className='fab'>
+		const dialogButtons = [
+							<FlatButton
+								label="Cancel"
+								primary={true}
+								onTouchTap={toggleDialog}
+							/>,
+							<FlatButton
+								label="Add" // "Submit"
+								primary={true}
+								keyboardFocused={true}
+								onTouchTap={toggleDialog}
+							/>
+						]
+
+		return 	<section>
+					<FloatingActionButton
+						onTouchTap={toggleDialog}
+						secondary={true}
+						className='fab'
+					>
 						<ContentAdd />
 					</FloatingActionButton>
 			        <Dialog
-			          title="Dialog With Actions"
-			          actions={actions}
+			          title="Add address"
+			          actions={dialogButtons}
 			          modal={false}
 			          open={state.open}
-			          onRequestClose={handleClose}
+			          onRequestClose={toggleDialog}
 			        >
-			          The actions in this window were passed in as an array of React objects.
+			            <form onSubmit={handleSubmit}>
+							{/* TODO add bitcoin regex to validate input (maybe server side has one?) */}
+							{/* NOTE check wallet-address-validator @ npm */}
+							{/* bitcoin regex == ^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$ */}
+							{/* TODO add more adddress types to validation */}
+							<TextField hintText="Address" errorText={state.addressError} fullWidth />
+							<Hide unless={state.userAccounts}>
+								<Subheader>Add address to:</Subheader>
+								<RadioButtonGroup name="shipSpeed" defaultSelected="existing">
+								  <RadioButton value="existing" label="Existing account" />
+								  <RadioButton value="new" label="New account" disabled={true} />
+								</RadioButtonGroup>
+								<SelectField
+									floatingLabelText="Select account"
+									value={this.state.value}
+									onChange={this.handleChange}
+								>
+									<MenuItem value="" primaryText="Choose account" disabled />
+									{state.userAccounts.map(
+										item => <MenuItem value={item._id} primaryText={item.name} />
+									)}
+								</SelectField>
+							</Hide>
+							<TextField hintText="Account name" errorText={state.nameError} fullWidth />
+							<RadioButtonGroup name="Account Type" defaultSelected="private">
+							  <RadioButton value="public" label="Public" />
+							  <RadioButton value="private" label="Private" />
+							</RadioButtonGroup>
+						</form>
 			        </Dialog>
-				</div>
+				</section>
 	}
 }
 
