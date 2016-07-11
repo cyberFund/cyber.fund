75c36dab769cdf629fd38ea258bd0faa017c008b
Mihail Paley <undeadlol1@mail.ru>
2016-07-12 04:46:27 +0500

    add unfinished AssetsManager

---
+++ imports/ui/components/AssetsManager.js
@@ -0,0 +1,74 @@
+import React, { PropTypes } from 'react'
+import { Grid, Cell, Icon, Button, Tabs, Tab } from 'react-mdl'
+import { _ } from 'meteor/underscore'
+import { If } from '../components/Utils'
+import ChaingearLink from '../components/ChaingearLink';
+import helpers from '../helpers'
+
+class AssetsManager extends React.Component {
+	constructor(params) {
+		super(params)
+		this.state = {
+			activeTab: 0,
+			// display tabs if there are alot of links
+			displayTabs: this.props.links.length > 8
+		}
+	}
+	render() {
+		const 	{ links, systemId } = this.props,
+				{ activeTab, displayTabs } = this.state,
+				{ linksWithTag, linksWithoutTags, existLinksWith } = helpers,
+				tags = ['Exchange', 'Wallet', 'Analytics', 'Magic']
+
+		if (_.isEmpty(links)) return null
+
+		function renderLinksArea() {
+			let linksArray = linksWithTag( links, tags[activeTab] )
+			// if tab == "Earn"
+			if (activeTab == 4) linksArray = linksWithoutTags(links, systemId)
+			return linksArray.map( (link, index)=> {
+				return 	<ChaingearLink link={link} key={index} />
+			})
+		}
+
+		return  <Grid className="text-center">
+					{/* TAB SELECTOR */}
+					<Cell col={12}>
+						<If condition={displayTabs}>
+							<Tabs
+								activeTab={activeTab}
+								onChange={activeTab => this.setState({activeTab})}
+								ripple
+							>
+								<If condition={existLinksWith(links, 'Exchange')} component={Tab}>
+									Buy
+								</If>
+								<If condition={existLinksWith(links, 'Wallet')} component={Tab}>
+									Hold
+								</If>
+								<If condition={existLinksWith(links, 'Analytics')} component={Tab}>
+									Analyze
+								</If>
+								<If condition={existLinksWith(links, 'Magic')} component={Tab}>
+									Magic
+								</If>
+								<If condition={linksWithoutTags(links, systemId)} component={Tab}>
+									Earn
+								</If>
+							</Tabs>
+						</If>
+					</Cell>
+					{/* CONTENT */}
+					<Cell col={12}>
+						  {renderLinksArea()}
+					</Cell>
+				</Grid>
+	}
+}
+
+AssetsManager.propTypes = {
+	links: PropTypes.array.isRequired,
+	systemId: PropTypes.string.isRequired
+}
+
+export default AssetsManager
