a9b6cb1ad2ef577c7158ed56cd1730a23cd620f7
Mihail Paley <undeadlol1@mail.ru>
2016-07-12 04:44:27 +0500

    (RatingPage) add ratingTable blaze component

--- imports/ui/pages/RatingPage.js
+++ imports/ui/pages/RatingPage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Grid, Cell } from 'react-mdl'
 import Hiring from '../components/Hiring'
 import RatingTableContainer from '../containers/RatingTableContainer'
+import Blaze from 'meteor/gadicc:blaze-react-component'
 
 const RatingPage = props => {
       return <Grid id="RatingPage">
                 <Cell col={12}> <Hiring /> </Cell>
-                <Cell col={12}> <RatingTableContainer /> </Cell>
+                {/*<Cell col={12}> <RatingTableContainer /> </Cell>*/}
+				<Cell col={12}> <Blaze template="ratingTable" /> </Cell>
               </Grid>
  }
 
