3fb32c70c8fbedac37836c791d32bc94a20a7141
Mihail Paley <undeadlol1@mail.ru>
2016-07-16 09:55:29 +0500

    (Top5AssetsContainer) fix wrong data fetching

--- imports/ui/containers/Top5AssetsContainer.js
+++ imports/ui/containers/Top5AssetsContainer.js
@@ -5,19 +5,20 @@ import Top5Assets from '../components/Top5Assets'
 
 export default createContainer( props => {
 
-	const selector = {
-		selector: {},
-		sort:{ "calculatable.RATING.sum": -1 },
-		limit: 5
-	}
-
-	Meteor.subscribe("currentDataRP", selector).ready()
-
+	const 	selector = 	{
+							selector: {},
+							sort: { "calculatable.RATING.sum": -1 } //,
+							// limit: 5
+						},
+			loaded 	= 	Meteor.subscribe("currentDataRP", selector).ready(),
+			systems = 	CurrentData.find({}, {
+							sort: { "calculatable.RATING.sum": -1 },
+							limit: 5
+						}).fetch()
+			console.log(systems)
 	return {
-		systems: CurrentData.find({}, {
-			sort: { "calculatable.RATING.sum": -1 },
-			limit: 5
-		}).fetch()
-}
+		loaded,
+		systems
+	}
 
 }, Top5Assets)
