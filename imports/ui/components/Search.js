02470a0d4d42938fec12f3ad0e81a01c6c033c62
Mihail Paley <undeadlol1@mail.ru>
2016-07-21 18:19:09 +0500

    create Search component

---
+++ imports/ui/components/Search.js
@@ -0,0 +1,42 @@
+import { Meteor } from 'meteor/meteor'
+import React, { PropTypes } from 'react'
+import { createContainer } from 'meteor/react-meteor-data'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import AutoComplete from 'material-ui/AutoComplete'
+import helpers from '../helpers'
+
+class Search extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { inputValue: '' }
+	}
+
+	handleSubmit (system) {
+		if(!system) return // TODO implement autoselect on unfinished input (ie 'bitco' + enter)
+		console.warn(system)
+		if (this.props.callback) this.props.callback(system)
+		else {
+			FlowRouter.go(`/system/${helpers._toUnderscores(system._id)}`)
+			this.setState({ inputValue: '' })
+		}
+	}
+
+	render() {
+		return 	<AutoComplete
+		          hintText="Search for coin"
+				  value={this.state.inputValue}
+		          dataSource={this.props.systems}
+				  dataSourceConfig={{ text: '_id', value: '_id' }}
+				  filter={AutoComplete.fuzzyFilter}
+				  maxSearchResults={5}
+				  onNewRequest={this.handleSubmit.bind(this)}
+				  {...this.props}
+		        />
+	}
+}
+
+export default createContainer(() => {
+	Meteor.subscribe("currentDataRP")
+	return { systems: CurrentData.find().fetch() }
+}, Search)
