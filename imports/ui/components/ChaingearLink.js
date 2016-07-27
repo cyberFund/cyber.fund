94ae4d180ce2b5d851e585b6022396269a18aa10
Mihail Paley <undeadlol1@mail.ru>
2016-07-27 19:29:25 +0500

    (ChaingearLink) add "card" property, move iconUrl() into helpers

--- imports/ui/components/ChaingearLink.js
+++ imports/ui/components/ChaingearLink.js
@@ -1,90 +1,35 @@
 import React, { PropTypes } from 'react'
-import { If, Else } from '../components/Utils'
+import Image from '../components/Image'
+import helpers from '../helpers'
 
 const ChaingearLink = props => {
 	const { link } = props
-	// returns specific fa-icon class
-	function iconUrl(link) {
-		//return "https://github.com/cyberFund/chaingear/blob/gh-pages/logos/" + link.icon;
-		switch (link.type) {
-		  case "wiki":
-			return "wikipedia-w";
-		  case "twitter":
-			return "twitter";
-		  case "wallet":
-			return "credit-card";
-		  case "reddit":
-			return "reddit";
-		  case "github":
-			return "github";
-		  case "blog":
-			return "pencil-square-o";
-		  case "website":
-			return "home";
-		  case "forum":
-			return "comments-o";
-		  case "explorer":
-			return "search";
-		  case "whitepaper":
-			return "mortar-board";
-		  case "paper":
-			return "graduation-cap";
-		  default:
-			break;
-		}
-		switch (link.icon.toLowerCase()) {
 
-		switch ( get(link, 'icon', ' ').toLowerCase() ) {
-		  case "wiki.png":
-			return "wikipedia-w";
-		  case "twitter.png":
-			return "twitter";
-		  case "wallet.png":
-			return "credit-card";
-		  case "reddit.png":
-			return "reddit";
-		  case "github.png":
-			return "github";
-		  case "blog.png":
-			return "pencil-square-o";
-		  case "website.png":
-			return "home";
-		  case "forum.png":
-			return "comments-o";
-		  case "explorer.png":
-			return "search";
-		  case "whitepaper.png":
-			return "mortar-board";
-		  case "paper.png":
-			return "graduation-cap";
-		  default:
-			break;
-		}
-		if (link.type == "custom") {
-		  return ""
-		}
-		return "external-link";
-	}
-	// TODO remove .cg-link?
+	let content
+	 // if card prop is specified render card with image
+	if (props.card)	content = 	<span className="text-large text-center cglinkcard-text">
+									<Image src={"https://static.cyber.fund/logos/" + link.icon} small />
+									&nbsp;{link.name}
+								</span>
+	// else render inline link
+	else 			content = 	<span>
+									<i className={`fa fa-${ helpers.iconUrl(link) || 'fa-external-link-square' }`}></i>
+									&nbsp;{link.name}
+								</span>
+								
 	return 	<a
 				href={link.url}
 				target="_blank"
 				style={{textDecoration: 'none'}}
-				className="cg-link"
 				{...props}
 			>
-				<If condition={iconUrl(link)}>
-			    	<i className={`fa fa-${iconUrl(link)}`}></i>
-			    </If>
-				<Else condition={iconUrl(link)}>
-    				<i className="fa fa-external-link-square"></i>
-    			</Else>
-				&nbsp;{link.name}
+				{content}
 			</a>
 }
 
 ChaingearLink.propTypes = {
-    link: PropTypes.object.isRequired
+    link: PropTypes.object.isRequired,
+	card: PropTypes.bool
 }
 
 export default ChaingearLink
--- imports/ui/helpers.js
+++ imports/ui/helpers.js
@@ -414,6 +414,66 @@ const helpers = {
 		  if (vUsd && vBtc) return `Ƀ${helpers.readableN4(vBtc)}/$${helpers.readableN2(vUsd)}` // ()
 		  if (vUsd || vBtc) return vBtc ? `Ƀ${helpers.readableN4(vBtc)}` : `$${helpers.readableN2(vUsd)}`
 		  return 0
+	  },
+
+	  // returns specific fa-icon class
+	  iconUrl: function(link) {
+		  //return "https://github.com/cyberFund/chaingear/blob/gh-pages/logos/" + link.icon;
+		  switch (link.type) {
+			case "wiki":
+			  return "wikipedia-w";
+			case "twitter":
+			  return "twitter";
+			case "wallet":
+			  return "credit-card";
+			case "reddit":
+			  return "reddit";
+			case "github":
+			  return "github";
+			case "blog":
+			  return "pencil-square-o";
+			case "website":
+			  return "home";
+			case "forum":
+			  return "comments-o";
+			case "explorer":
+			  return "search";
+			case "whitepaper":
+			  return "mortar-board";
+			case "paper":
+			  return "graduation-cap";
+			default:
+			  break;
+		  }
+
+		  switch ( get(link, 'icon', ' ').toLowerCase() ) {
+			case "wiki.png":
+			  return "wikipedia-w";
+			case "twitter.png":
+			  return "twitter";
+			case "wallet.png":
+			  return "credit-card";
+			case "reddit.png":
+			  return "reddit";
+			case "github.png":
+			  return "github";
+			case "blog.png":
+			  return "pencil-square-o";
+			case "website.png":
+			  return "home";
+			case "forum.png":
+			  return "comments-o";
+			case "explorer.png":
+			  return "search";
+			case "whitepaper.png":
+			  return "mortar-board";
+			case "paper.png":
+			  return "graduation-cap";
+			default:
+			  break;
+		  }
+		  if (link.type == "custom") return ''
+		  return "external-link";
 	  }
 
 }
