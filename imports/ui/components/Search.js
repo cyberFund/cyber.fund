ab93d6bd70ba0484319e4fcf2471f8f7004eb36f
Mihail Paley <undeadlol1@mail.ru>
2016-07-29 23:40:09 +0500

    (Search) change text color, refactoring
    removed alot of state functionality. It's because turned they do
    nothing.

--- imports/ui/components/Search.js
+++ imports/ui/components/Search.js
@@ -4,34 +4,31 @@ import { createContainer } from 'meteor/react-meteor-data'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import AutoComplete from 'material-ui/AutoComplete'
 import helpers from '../helpers'
-
+// TODO add comments. Things are not obvious at all
 class Search extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = { inputValue: '' }
-	}
-
-	handleSubmit (system) {
+	handleSubmit(system) {
 		if(!system) return // TODO implement autoselect on unfinished input (ie 'bitco' + enter)
 		console.warn(system)
 		if (this.props.callback) this.props.callback(system)
-		else {
-			FlowRouter.go(`/system/${helpers._toUnderscores(system._id)}`)
-			this.setState({ inputValue: '' })
-		}
+		else FlowRouter.go(`/system/${helpers._toUnderscores(system._id)}`)
 	}
 
 	render() {
+		const 	{ state, props, handleSubmit } = this,
+				colorWhite = { color: 'white' }
+
 		return 	<AutoComplete
 		          hintText="Search for coin"
-				  value={this.state.inputValue}
-		          dataSource={this.props.systems}
+		          dataSource={props.systems}
+				  // 'text' tells which property to scan, 'value' tells what to send in callback
 				  dataSourceConfig={{ text: '_id', value: '_id' }}
 				  filter={AutoComplete.fuzzyFilter}
 				  maxSearchResults={5}
-				  onNewRequest={this.handleSubmit.bind(this)}
-				  {...this.props}
+				  onNewRequest={handleSubmit.bind(this)}
+				  hintStyle={colorWhite}
+				  inputStyle={colorWhite}
+				  {...props}
 		        />
 	}
 }
