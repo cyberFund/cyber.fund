baffd3641d13ab782677f1bac7b64814e4e3518c
Mihail Paley <undeadlol1@mail.ru>
2016-08-01 00:40:22 +0500

    (ProfileLink) create component

---
+++ imports/ui/components/ProfileLink.js
@@ -0,0 +1,34 @@
+import React, { PropTypes } from 'react'
+import get from 'oget'
+
+// usage example:
+// <ProfileLink /> // link to currently logged in user
+// or
+// <ProfileLink user={someUsersObject} />
+
+const ProfileLink = props => {
+
+	const	avatar = get(props.user, 'avatar', ''),
+			username = get(props.user, 'username', '')
+
+	return 	<a href={`/@${username}`} className="mdl-navigation__link">
+				<img
+					src={avatar}
+					alt={`${username}'s avatar`}
+					className="mdl-list__item-avatar"
+					style={{marginRight: '10px'}}
+				/>
+				{username}
+	        </a>
+
+}
+
+ProfileLink.defaultProps = {
+	user: Meteor.user()
+}
+
+ProfileLink.propTypes = {
+	user: PropTypes.object
+}
+
+export default ProfileLink
