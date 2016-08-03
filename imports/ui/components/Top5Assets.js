50d1461a5fbd90a368580392202a9ef30e9e1773
Mihail Paley <undeadlol1@mail.ru>
2016-08-04 03:05:15 +0500

    (Top5Assets) reduce header margin, refactoring

--- imports/ui/components/Top5Assets.js
+++ imports/ui/components/Top5Assets.js
@@ -6,11 +6,12 @@ import Image from '../components/Image'
 import helpers from '../helpers'
 
 const Top5Assets = (props) => {
+	
 	// render rows with data
   	function renderRows() {
 		return props.systems.map( system => {
-			const months = get(system, 'calculatable.RATING.vector.GR.months')
-			const monthlyGrowthD = get(system, 'calculatable.RATING.vector.GR.monthlyGrowthD')
+			const 	months = get(system, 'calculatable.RATING.vector.GR.months')
+					monthlyGrowthD = get(system, 'calculatable.RATING.vector.GR.monthlyGrowthD')
 			return  <tr key={system._id}>
 						<td className="mdl-data-table__cell--non-numeric">
 							<a href={`/system/${helpers._toUnderscores(system._id)}`}>
@@ -34,19 +35,38 @@ const Top5Assets = (props) => {
 					</tr>
 		})
 	}
+
     // TOOLTIPS TEXT
 	// <br/> is the way MDL implements multiline tooltips
-    const cmgrTP = <span>Key indicator that shows<br/> long term profitability of<br/> investment. We use monthly<br/> calculation in opposite to<br/> annual in traditional finance<br/> as blockchain markets are<br/> faster than tradational thus<br/> should be evaulated more<br/> frequently.</span>
-	const monthsTP = <span>This is time passed since<br/> time of sampling 'first price'<br/> metric.</span>
-	const ratingTP = <span>Compound evaluation of a<br/> given crypto property.<br/>Methodology depend on<br/> a stage, type and class<br/> of a given cryptoproperty.<br/>More than 50 indicators are<br/> evaluated in a realtime.</span>
+    const 	cmgrTP = 	<span>
+							Key indicator that shows
+							<br/> long term profitability of
+							<br/> investment. We use monthly
+							<br/> calculation in opposite to
+							<br/> annual in traditional finance
+							<br/> as blockchain markets are
+							<br/> faster than tradational thus
+							<br/> should be evaulated more
+							<br/> frequently.
+						</span>,
+			ratingTP = 	<span>
+							Compound evaluation of a
+							<br/> given crypto property.
+							<br/>Methodology depend on
+							<br/> a stage, type and class
+							<br/> of a given cryptoproperty.
+							<br/>More than 50 indicators are
+							<br/> evaluated in a realtime.
+						</span>,
+			monthsTP = 	<span>
+							This is time passed since
+							<br/> time of sampling 'first price'
+							<br/> metric.
+						</span>
 
     return (
       <div>
-        <Grid>
-          <Cell col={12}>
-            <h5 className="center"> Top 5 Rated Assets </h5>
-          </Cell>
-        </Grid>
+        <h5 className="center"> Top 5 Rated Assets </h5>
         <Grid>
 			<Cell col={12}>
 				<table className="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp center" {...props}>
@@ -83,7 +103,9 @@ const Top5Assets = (props) => {
       </div>
   )
 }
+
 Top5Assets.propTypes = {
   systems: PropTypes.array.isRequired
 }
+
 export default Top5Assets
