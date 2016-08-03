3af97aab65df6bb9f41a586d8d1d27a513793467
Mihail Paley <undeadlol1@mail.ru>
2016-08-04 03:14:09 +0500

    (SystemLink) add "card" property

--- imports/ui/components/SystemLink.js
+++ imports/ui/components/SystemLink.js
@@ -3,9 +3,18 @@ import { Card } from 'react-mdl'
 import Image from '../components/Image'
 import helpers from '../helpers'
 
+// usage example:
+// <SystemLink
+// 	 system={Object}
+// 	 card // optional
+// />
+
 const SystemLink = props => {
-	const 	{ system } = props,
-			innerStyle = {
+
+	// data
+	const 	{ system } = props
+	// styles
+	const	innerStyle = {
 	                display: 'inline-flex',
 	                flexDirection: 'row',
 	                alignItems: 'center',
@@ -27,23 +36,33 @@ const SystemLink = props => {
 			},
 			linkStyle = {color: 'inherit', textDecoration: 'none'}
 
-	return 	<a href={`/system/${helpers._toUnderscores(system._id)}`} style={linkStyle}>
-				<Card className="hover-shadow" shadow={2} style={{minHeight: 'inherit', width: 'auto'}}>
-					<div style={innerStyle}>
-						<img
-							style={imgStyle}
-							src={CF.Chaingear.helpers.cgSystemLogoUrl(system)}
-							alt={system._id + 'logo'}
-						/>
-						<span style={textStyle} className="text-large">{system._id}</span>
-					</div>
-				</Card>
-			</a>
+	// render link as card or inline
+	if (props.card)	return 	<a href={`/system/${helpers._toUnderscores(system._id)}`} style={linkStyle}>
+								<Card className="hover-shadow" shadow={2} style={{minHeight: 'inherit', width: 'auto'}}>
+									<div style={innerStyle}>
+										<img
+											style={imgStyle}
+											src={CF.Chaingear.helpers.cgSystemLogoUrl(system)}
+											alt={system._id + 'logo'}
+										/>
+										<span style={textStyle} className="text-large">{system._id}</span>
+									</div>
+								</Card>
+							</a>
+
+	else 			return	<a href={`/system/${helpers._toUnderscores(system._id)}`}>
+								<Image
+									src={system}
+									style={{marginRight: 24}}
+									avatar
+								/>
+								<span>{helpers.displaySystemName(system)}</span>
+							</a>
 }
 
 SystemLink.propTypes = {
-    system: PropTypes.object.isRequired,
-	card: PropTypes.bool // TODO add exaplanation
+	card: PropTypes.bool,
+    system: PropTypes.object.isRequired
 }
 
 export default SystemLink
