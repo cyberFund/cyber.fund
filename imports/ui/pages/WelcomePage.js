c4a7de2e395003a6aafc69c8de584743ac947a73
Mihail Paley <undeadlol1@mail.ru>
2016-08-04 03:22:43 +0500

    (WelcomePage) add cards to info sections, refactoring

--- imports/ui/pages/WelcomePage.js
+++ imports/ui/pages/WelcomePage.js
@@ -16,9 +16,10 @@ class WelcomePage extends React.Component {
     componentDidMount() {
         // self explanatory
         $('[href="/sign-in"]').on('click', () => {
-            analytics.track('redirect to Sign In', {
-              from: 'welcome'
-            })
+            analytics.track(
+				'redirect to Sign In',
+				{ from: 'welcome' }
+			)
             FlowRouter.go('/sign-in')
         })
         // make drawer button white
@@ -26,7 +27,8 @@ class WelcomePage extends React.Component {
         // TODO create states for drawer button in MainLayout and apply changes through state manipulation
         $('.mdl-layout__drawer-button').css('color', 'white !important')
     }
-    componentWillUnmount() { // clean up
+	// clean up
+    componentWillUnmount() {
         $('[href="/sign-in"]').remove()
         $('.mdl-layout__drawer-button').css('color', 'black')
     }
@@ -38,54 +40,54 @@ class WelcomePage extends React.Component {
             backgroundSize: 'cover',
             minHeight: '300px',
             padding: '60px 0'
-        }
+        },
+		cardClasses = 'mdl-card mdl-shadow--4dp hover'
+
         return (
                 <div id="WelcomePage" className="text-center">
+					{/* FIRST MAIN SECTION */}
                     <Grid style={wideSectionStyle}>
                         <Cell col={8} tablet={5} phone={12}>
                             <h1><Brand /></h1>
                             <h3>Where the World Discovers Blockchain Systems</h3>
                         </Cell>
                         <Cell col={4} tablet={3} phone={12}>
-                            <span>
-                                <h5>Control Your Portfolio
-                                    <p>Follow Your Favourite Cryptocurrencies</p>
-                                    <p>Keep Records of Your Public Keys</p>
-                                    <p>Follow the Best Portfolios</p>
-                                </h5>
-                            </span>
+                            <h5>Control Your Portfolio</h5>
+                            <p>Follow Your Favourite Cryptocurrencies</p>
+                            <p>Keep Records of Your Public Keys</p>
+                            <p>Follow the Best Portfolios</p>
                             <Button href="/sign-in" raised colored ripple>Sign Up with Twitter</Button>
                         </Cell>
                     </Grid>
-                    <Grid>
-                        <Cell col={12}>
-                             <h3>How cyber <span className="red-text">•</span> Fund helps you manage your blockchain portfolio</h3>
-                        </Cell>
-                    </Grid>
+
+					{/* VIDEO */}
+					<h3>How cyber <span className="red-text">•</span> Fund helps you manage your blockchain portfolio</h3>
                     <div className="video-container">
                         <iframe width="853" height="480" src="//www.youtube.com/embed/gGbbawSJ6P0?rel=0" frameborder="0" allowfullscreen>
                         </iframe>
                     </div>
+
+
                     <Grid>
                         <Cell col={12}>
                             <h4>Sign up now and your public account will be free forever!</h4>
                             <Button href="/sign-in" raised accent ripple>Sign Up</Button>
                         </Cell>
                     </Grid>
-                    <hr/>
+                    <hr />
                     <Grid>
                         <Cell col={12}><h2>Why you’ll love <Brand />.</h2></Cell>
-                        <Cell col={4} tablet={4} phone={12}>
+                        <Cell col={4} tablet={4} phone={12} className={cardClasses}>
                             <i style={{color: '#ff5722', fontSize: '6em'}} className="material-icons">directions_walk</i>
                             <h4>Realtime Valuation</h4>
                             <p>We crunch market data on all blockchains and even for some notable illiquid blockchain assets.</p>
                         </Cell>
-                        <Cell col={4} tablet={4} phone={12}>
+                        <Cell col={4} tablet={4} phone={12} className={cardClasses}>
                             <i style={{color: '#ffeb3b', fontSize: '6em'}} className="material-icons">directions_run</i>
                             <h4>Balance Autoupdate</h4>
                             <p>A majority of the blockchains' balances are updated automatically. Insert your public address and it will be tracked.</p>
                         </Cell>
-                        <Cell col={4} tablet={4} phone={12}>
+                        <Cell col={4} tablet={4} phone={12} className={cardClasses}>
                             <i style={{color: '#4CAF50', fontSize: '6em'}} className="material-icons">directions_bike</i>
                             <h4>Trustless</h4>
                             <p>We invest a lot in blockains too, so we care about your security. We never touch your private keys.</p>
@@ -93,18 +95,18 @@ class WelcomePage extends React.Component {
 
                         <br className="clearfix mdl-cell--hide-tablet mdl-cell--hide-phone" />
 
-                        <Cell col={4} tablet={4} phone={12}>
+                        <Cell col={4} tablet={4} phone={12} className={cardClasses}>
                             <i style={{color: '#2196F3', fontSize: '6em'}} className="material-icons">router</i>
                             <h4>Scanner</h4>
                             <p>We scan all(!) blockchain space for prominent crowdfunding and huge deals.</p>
                         </Cell>
-                        <Cell col={4} tablet={4} phone={12}>
+                        <Cell col={4} tablet={4} phone={12} className={cardClasses}>
                             <i style={{color: '#cddc39', fontSize: '6em'}} className="material-icons">toys</i>
                             <h4>Dispatcher</h4>
                             <p>We filter the noise so only awesome digital
                             assets reach your attention. No scum. No kitchen forks. No middleman.</p>
                         </Cell>
-                        <Cell col={4} tablet={4} phone={12}>
+                        <Cell col={4} tablet={4} phone={12} className={cardClasses}>
                             <i style={{color: '#cddc39', fontSize: '6em'}} className="material-icons">grade</i>
                             <h4>Verifier</h4>
                             <p>We make due diligence free. Really. View all of our
@@ -112,7 +114,7 @@ class WelcomePage extends React.Component {
                             Take a look.</p>
                         </Cell>
                     </Grid>
-                    <hr/>
+                    <hr />
                     <Grid>
                         <Cell col={12}>
                             <h4>Welcome to <Brand />!</h4>
@@ -122,12 +124,12 @@ class WelcomePage extends React.Component {
                             <p>{helpers.usersCount()} people already joined</p>
                         </Cell>
                     </Grid>
-                    <hr/>
+                    <hr />
                     <Grid>
                         <Cell col={12}><h2>For Developers</h2></Cell>
                     </Grid>
                     <Grid>
-                        <Cell col={6} tablet={4} phone={12}>
+                        <Cell col={6} tablet={4} phone={12} className={cardClasses}>
                             <i style={{color: '#ffeb3b', fontSize: '6em'}} className="material-icons">stars</i>
                             <h4>Rating</h4>
                             If you are a developer you can add cryptocurrency or cryptoasset yourself using our opensource Chaingear library.
@@ -135,12 +137,12 @@ class WelcomePage extends React.Component {
                                 <Button raised colored ripple href="http://cybertalks.org/t/rating-listing-of-cryptocurrencies-and-cryptoassets/353" target="blank">Rating listing</Button>
                             </p>
                         </Cell>
-                        <Cell col={6} tablet={4} phone={12}>
+                        <Cell col={6} tablet={4} phone={12} className={cardClasses}>
                             <i style={{color: '#F44336', fontSize: '6em'}} className="material-icons">directions_run</i>
                             <h4>Radar</h4>
                             You excited about creating or already working on blockchain project you have awesome option to list your project in Radar section of cyber•Fund.
                             <p>
-                            <Button raised colored ripple href="http://cybertalks.org/t/radar-listing-of-cryptocurrencies-and-cryptoassets/354" target="blank">Radar listing</Button>
+                            	<Button raised colored ripple href="http://cybertalks.org/t/radar-listing-of-cryptocurrencies-and-cryptoassets/354" target="blank">Radar listing</Button>
                             </p>
                         </Cell>
                     </Grid>
