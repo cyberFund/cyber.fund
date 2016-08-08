10ea534e22b1c4489333faec70fdffc63c8f60a4
Mihail Paley <undeadlol1@mail.ru>
2016-08-08 06:17:19 +0500

    (PageLoading) rework component
    turns out i was wrong and difference is close to none. Plus previous way
    introduced too many bugs and inconsistencies. Now it's plain and simple

--- imports/ui/higherOrderComponents/PageLoading.js
+++ imports/ui/higherOrderComponents/PageLoading.js
@@ -1,31 +1,19 @@
 import React from 'react'
 import Loading from '../components/Loading'
 
-// this is a wrapper for page components which shows a loading indicator on load.
-// to make page load faster this trick was used:
-// both Loading and Page component are being rendered but Page is invisible untill data is ready.
-// this is important because usually we wait for data => and only afterwards we render component
-// the difference between two methods can be up to 2000 miliseconds(!) of longer loading time
+// this is a wrapper for page components which shows a loading indicator on load
 
 function PageLoading(PageComponent) {
 
-	class Enhance extends React.Component {
+	return class Enhance extends React.Component {
 
 		render() {
-
 			const { props, state } = this
-			return  <div>
-						{ props.loaded ? null : <Loading /> }
-						<div className={ props.loaded ? 'visible' : 'hidden' }>
-							<PageComponent {...state} {...props} />
-						</div>
-					</div>
-
+			return props.loaded ? <PageComponent {...state} {...props} /> : <Loading />
 		}
 
 	}
 
-	return Enhance
 }
 
 export default PageLoading
