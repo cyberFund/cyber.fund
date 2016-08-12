4dce8b365c1f6b6224d00376c591598750b5f9ff
Mihail Paley <undeadlol1@mail.ru>
2016-08-12 14:15:24 +0500

    (FundsTable) fix responsive styles, refactor

--- imports/ui/components/FundsTable.js
+++ imports/ui/components/FundsTable.js
@@ -4,10 +4,18 @@ import get from 'oget'
 import helpers from '../helpers'
 import Image from '../components/Image'
 
+// usage example:
+// <FundsTable funds={Array} />
+
 const FundsTable = props => {
+
+	// variables
+	const 	tableClasses = "mdl-data-table mdl-js-data-table mdl-shadow--2dp center",
+			nonNumeric 	 = "mdl-data-table__cell--non-numeric"
+
     const renderRows = props.funds.map( user => {
-                return  <tr key={user._id} itemscope itemtype="http://schema.org/Person">
-                            <td className="mdl-data-table__cell--non-numeric">
+                return  <tr key={user._id} itemScope itemType="http://schema.org/Person">
+                            <td className={nonNumeric}>
                                 <a href={`/@${user.username}`} title={user.profile.name}>
                                     <Image
                                         avatar
@@ -29,13 +37,10 @@ const FundsTable = props => {
                         </tr>
     })
 
-    const tableStyle = {width: 545}
-    const tableClasses = "mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp center"
-
-    return <table style={tableStyle} className={tableClasses} {...props}>
+    return <table className={tableClasses} {...props}>
               <thead>
                     <tr>
-                      <th style={{textAlign: 'center'}} className="mdl-data-table__cell--non-numeric">
+                      <th style={{textAlign: 'center'}} className={nonNumeric}>
                           Fund
                       </th>
                       <th>Valuation in Ƀ</th>
