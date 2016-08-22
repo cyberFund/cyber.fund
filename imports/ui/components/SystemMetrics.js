5dd0ab6856936a39a2b61213d48e55ef372edf9b
Mihail Paley <undeadlol1@mail.ru>
2016-08-22 14:28:20 +0200

    (SystemMetrics)(WIP) trying to fix undefined system properties

--- imports/ui/components/SystemMetrics.js
+++ imports/ui/components/SystemMetrics.js
@@ -2,10 +2,13 @@ import React, { PropTypes } from 'react'
 import { If, Show } from '../components/Utils'
 import { Grid, Cell } from 'react-mdl'
 import helpers from '../helpers'
+import get from 'oget'
+
 // TODO add comments, refactoring
 const SystemMetrics = props =>{
-	const 	{ metrics } = props.system
 
+	const 	{ metrics } = props.system
+	if(!metrics.price) return null
 	let 	todayVolumeUsd = 0,
 			yesterdayVolumeUsd = 0,
 			todayVolumeBtc = 0,
@@ -32,122 +35,129 @@ const SystemMetrics = props =>{
 		// do nothing
 	}
 
-	function renderGreenOrRedNumber(number) {
+	function greenOrRedNumber(number) {
 		if (!number) return '&nbsp;'
 		return	<span className={helpers.greenRedNumber(number)}>
 					{ helpers.percentsToTextUpDown(number, 2) }
 				</span>
 	}
-
+	console.warn(metrics)
+	console.warn(metrics.price)
+	console.warn(Boolean(metrics.price))
+	// when you pass () into If component it will throw an error,
+	// because function tries to read data before processing it.
+	const 	priceUsd = get(metrics, 'price.usd'),
+			priceBtc = get(metrics, 'price.btc'),
+			capUsd = get(metrics, 'cap.usd'),
+			capBtc = get(metrics, 'capBtc')
+	console.warn(priceUsd, priceBtc)
+	console.warn(capUsd, capBtc)
     return 	<Grid className="text-center">
-		<Cell col={3} tablet={4} phone={4} shadow={2} className="mdl-card">
+				<Cell col={3} tablet={4} phone={4} shadow={2} className="mdl-card">
 
 				{/* PRICE */}
-				<If condition={metrics.price}> {/* do we need this check? */}
-						<If condition={metrics.price.usd || metrics.price.btc}> {/* do we need this check? */}
-							<h4>Price</h4>
-							<If condition={metrics.price.usd}>
-								<p>
-									<span itemScope itemProp="offers" itemType="http://schema.org/Offer" className="text-large">
-										<span itemProp="priceCurrency" content="USD">$&nbsp;</span>
-										<span  itemProp="price" content={metrics.price.usd}>
-											{/* add normal comparison? */}
-											{
-												helpers.gte(metrics.price.usd, 0.1)
-												? helpers.readableN2(metrics.price.usd)
-												: helpers.satoshi_decimals(metrics.price.usd, 6)
-											}
-										</span>
-							        </span>
-									<br />
-									{renderGreenOrRedNumber(metrics.priceChangePercents.day.usd)}
-								</p>
-							</If>
-							<If condition={metrics.price.btc}>
-								<p>
-									<span itemProp="offers" itemScope itemType="http://schema.org/Offer" className="text-large">
-										<span itemProp="priceCurrency" content="BTC">Ƀ&nbsp;</span>
-										<span itemProp="price" content={metrics.price.btc}>
-											{helpers.satoshi_decimals(metrics.price.btc, 8)}
-										</span>
-							        </span>
-									<br />
-									<If condition={metrics.priceChangePercents.day.btc}>
-										{renderGreenOrRedNumber(metrics.priceChangePercents.day.btc)}
-									</If>
-								</p>
-							</If>
-						</If>
-				</If>
+					<Show condition={priceUsd || priceBtc}>
+						{ console.warn('THIS MUST NOT BE SHOWN')}
+						<h4>Price</h4>
+						<Show condition={priceUsd}>
+							<p>
+								<span itemScope itemProp="offers" itemType="http://schema.org/Offer" className="text-large">
+									<span itemProp="priceCurrency" content="USD">$&nbsp;</span>
+									<span  itemProp="price" content={priceUsd}>
+										{/* add normal comparison? */}
+										{
+											helpers.gte(priceUsd, 0.1)
+											? helpers.readableN2(priceUsd)
+											: helpers.satoshi_decimals(priceUsd, 6)
+										}
+									</span>
+						        </span>
+								<br />
+								{greenOrRedNumber(get(metrics, 'priceChangePercents.day.usd', 0))}
+							</p>
+						</Show>
+						<Show condition={priceBtc}>
+							<p>
+								<span itemProp="offers" itemScope itemType="http://schema.org/Offer" className="text-large">
+									<span itemProp="priceCurrency" content="BTC">Ƀ&nbsp;</span>
+									<span itemProp="price" content={priceBtc}>
+										{helpers.satoshi_decimals(priceBtc, 8)}
+									</span>
+						        </span>
+								<br />
+								<Show condition={metrics.priceChangePercents.day.btc}>
+									{greenOrRedNumber(metrics.priceChangePercents.day.btc)}
+								</Show>
+							</p>
+						</Show>
+					</Show>
 			</Cell>
 
 			{/* CAP */}
 			<Cell col={3} tablet={4} phone={4} shadow={2} className="mdl-card">
-				<If condition={metrics.cap}> {/* do we need this check? */}
-						<If condition={metrics.cap.usd || metrics.cap.btc}> {/* do we need this check? */}
-							<h4>Cap</h4>
-							<If condition={metrics.cap.usd}>
-								<p>
-									<span className="text-large">
-										{'$ ' + helpers.readableN0(metrics.cap.usd)}
-									</span>
-									<br />
-									{renderGreenOrRedNumber(metrics.capChangePercents.day.usd)}
-								</p>
-							</If>
-							<If condition={metrics.cap.btc}>
-								<p>
-									<span className="text-large">
-										{'Ƀ ' + helpers.readableN0(metrics.cap.btc)}
-									</span>
-									<br />
-									{renderGreenOrRedNumber(metrics.capChangePercents.day.btc)}
-								</p>
-							</If>
-						</If>
-				</If>
+				<Show condition={capUsd || capBtc }> {/* do we need this check? */}
+					<h4>Cap</h4>
+					<Show condition={capUsd}>
+						<p>
+							<span className="text-large">
+								{'$ ' + helpers.readableN0(capUsd)}
+							</span>
+							<br />
+							{greenOrRedNumber(metrics.capChangePercents.day.usd)}
+						</p>
+					</Show>
+					<Show condition={capBtc}>
+						<p>
+							<span className="text-large">
+								{'Ƀ ' + helpers.readableN0(capBtc)}
+							</span>
+							<br />
+							{greenOrRedNumber(metrics.capChangePercents.day.btc)}
+						</p>
+					</Show>
+				</Show>
 			</Cell>
 
 			{/* TRADE */}
 			<Cell col={3} tablet={4} phone={4} shadow={2} className="mdl-card">
-				<If condition={todayVolumeUsd}>
-						<h4>Trade</h4>
-						<p>
-							<span className="text-large">
-								{'$ ' + helpers.readableN0(todayVolumeUsd)}
-							</span>
-							<br />
-							{renderGreenOrRedNumber(usdVolumeChange)}
-						</p>
-						<p>
-							<span className="text-large">
-								{'Ƀ ' + helpers.readableN0(todayVolumeBtc)}
-							</span>
-							<br />
-							{renderGreenOrRedNumber(btcVolumeChange)}
-						</p>
-				</If>
+				<Show condition={todayVolumeUsd}>
+					<h4>Trade</h4>
+					<p>
+						<span className="text-large">
+							{'$ ' + helpers.readableN0(todayVolumeUsd)}
+						</span>
+						<br />
+						{greenOrRedNumber(usdVolumeChange)}
+					</p>
+					<p>
+						<span className="text-large">
+							{'Ƀ ' + helpers.readableN0(todayVolumeBtc)}
+						</span>
+						<br />
+						{greenOrRedNumber(btcVolumeChange)}
+					</p>
+				</Show>
 			</Cell>
 
 			{/* SUPPLY */}
 			<Cell col={3} tablet={4} phone={4} shadow={2} className="mdl-card">
-				<If condition={metrics.supply}>
-						<h4>Supply</h4>
-						<p>
-							<span className="text-large">
-								{helpers.readableN0(metrics.supply)}
-								&nbsp;
-								{helpers.displayCurrencyName(props.system) + 's'}
-							</span>
-							<br />
-							{renderGreenOrRedNumber(metrics.supplyChangePercents.day)}
-						</p>
-						<If condition={metrics.supplyChange} component='p'>
-							{ helpers.withSign(metrics.supplyChange.day) }
+				<Show condition={metrics.supply}>
+					<h4>Supply</h4>
+					<p>
+						<span className="text-large">
+							{helpers.readableN0(metrics.supply)}
 							&nbsp;
-							{ helpers.displayCurrencyName(props.system) + 's' }
-						</If>
-				</If>
+							{helpers.displayCurrencyName(props.system) + 's'}
+						</span>
+						<br />
+						{greenOrRedNumber(metrics.supplyChangePercents.day)}
+					</p>
+					<Show condition={metrics.supplyChange} component='p'>
+						{ helpers.withSign(metrics.supplyChange.day) }
+						&nbsp;
+						{ helpers.displayCurrencyName(props.system) + 's' }
+					</Show>
+				</Show>
 			</Cell>
 		</Grid>
 }
