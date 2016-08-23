d040a5ad4e6c6c3b97c45b51563505d86a185e60
Mihail Paley <undeadlol1@mail.ru>
2016-08-23 12:14:06 +0200

    (mobile-config.js)(WIP) first draft

---
+++ mobile-config.js
@@ -0,0 +1,37 @@
+App.info({
+  id: 'fund.cyber', // com.company.somepage
+  name: 'cyber.fund',
+  // version: "0.0.1"
+});
+App.setPreference('BackgroundColor', '0xff0000ff');
+// This section sets up some basic app metadat	a,
+// the entire section is optional.
+// App.info({
+//   id: 'com.example.matt.uber',
+//   name: 'über',
+//   description: 'Get über power in one button click',
+//   author: 'Matt Development Group',
+//   email: 'contact@example.com',
+//   website: 'http://example.com'
+// });
+// Set up resources such as icons and launch screens.
+// App.icons({
+//   'iphone': 'icons/icon-60.png',
+//   'iphone_2x': 'icons/icon-60@2x.png',
+//   // ... more screen sizes and platforms ...
+// });
+// App.launchScreens({
+//   'iphone': 'splash/Default~iphone.png',
+//   'iphone_2x': 'splash/Default@2x~iphone.png',
+//   // ... more screen sizes and platforms ...
+// });
+// // Set PhoneGap/Cordova preferences
+// App.setPreference('BackgroundColor', '0xff0000ff');
+// App.setPreference('HideKeyboardFormAccessoryBar', true);
+// App.setPreference('Orientation', 'default');
+// App.setPreference('Orientation', 'all', 'ios');
+// // Pass preferences for a particular PhoneGap/Cordova plugin
+// App.configurePlugin('com.phonegap.plugins.facebookconnect', {
+//   APP_ID: '1234567890',
+//   API_KEY: 'supersecretapikey'
+// });
