d4618f5cd4140480b6b57961a9362297e8d36a2d
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-10-31 22:52:54 +0200

    added metrics to crowdsale roport

--- server/lib/routes/accounts.js
+++ server/lib/routes/accounts.js
@@ -167,7 +167,7 @@ Picker.route('/api03/username/:username/accounts', function(params, req, res, ne
 Picker.route('/api03/crowdsale/:system_name', function(params, req, res, next) {
     var options = normalizeOptions(url.parse(req.url, true).query);
     var getSystem = function(system_name){
-      return CurrentData.findOne({_id: system_name.replace(/\_/g, ' ')}, {fields: {crowdsales: 1}}) || {
+      return CurrentData.findOne({_id: system_name.replace(/\_/g, ' ')}, {fields: {crowdsales: 1, metrics: 1}}) || {
         error: "not found"
       };
     }
